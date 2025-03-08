afef7fddc476d1cd9df19aba50f2db399cd1d5ed
taif-k <taifk817@gmail.com>
2025-03-08 10:23:41 +0530

    update

--- C/c_fundamental/Practise/unique_numbers.c
+++ C/c_fundamental/Practise/unique_numbers.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
 
+void unique_numbers(int numb[10], char uniq[10]);
+
 int main()
 {
-    int numbers[10] = {1, 1, 4, 5, 5, 0, 2, 3, 7, 7};
-    int unique[10];
-    int counter = 0;
+    int numbers[10] = {1, 1, 4, 5, 5, -1, 2, 3, -1, 7};
+    char unique[10];
 
+    unique_numbers(numbers, unique);
+    return 0;
+}
+
+void unique_numbers(int numb[10], char uniq[10])
+{
+    int counter = 0;
     for (int i = 0; i < 10; i++)
     {
         int match = 0;
         for (int j = i + 1; j < 10; j++)
         {
-            if (numbers[i] == numbers[j])
+            if (numb[i] == numb[j])
             {
                 match = 1;
                 break;
@@ -23,7 +31,7 @@ int main()
             int count = 0;
             for (int k = 0; k < 10; k++)
             {
-                if (numbers[i] == unique[k])
+                if (numb[i] == numb[k])
                 {
                     count = 1;
                     break;
@@ -31,7 +39,7 @@ int main()
             }
             if (count == 0)
             {
-                unique[counter] = numbers[i];
+                uniq[counter] = numb[i];
                 counter++;
             }
         }
@@ -42,7 +50,7 @@ int main()
         int match = 0;
         for (int j = 0; j < 10; j++)
         {
-            if (numbers[i] == unique[j])
+            if (numb[i] == uniq[j])
             {
                 match = 1;
                 break;
@@ -51,15 +59,13 @@ int main()
 
         if (match == 0)
         {
-            unique[counter] = numbers[i];
+            uniq[counter] = numb[i];
             counter++;
         }
     }
 
     for (int i = 0; i < counter; i++)
     {
-        printf(" %d ", unique[i]);
+        printf(" %d ", uniq[i]);
     }
-
-    return 0;
 }

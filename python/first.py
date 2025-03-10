5832db250c202a1c6166da0a0598c3aa3f391364
taif-k <taifk817@gmail.com>
2025-03-10 11:34:40 +0530

    play with arguments and parameters

--- python/first.py
+++ python/first.py
@@ -1,11 +1,14 @@
-def calculation():
+def calculation(alphabetD):
     a = 1
     b = 3
+    print("D is ", alphabetD)
     return a + b
 
 def main():
-    
-    print("Sum is ",calculation())
+    c = 2
+    d = 8
+    print("C is",c)
+    print("Sum is ",calculation(d))
 
 main() 
 

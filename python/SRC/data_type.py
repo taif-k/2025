34f88b26531e073a9789b487cb53ac5ee9d6d600
taif-k <taifk817@gmail.com>
2025-03-12 09:51:43 +0530

    add comment

--- python/SRC/data_type.py
+++ python/SRC/data_type.py
@@ -3,6 +3,7 @@ name = "taif"
 country = "India"
 info = f"""Hi {name}, I am using doc string for practise """
 print(info)
+print("{name}") # cant use {name} in double qoutes except f"{name}"
 
 # dictionary
 location = {"city":"New Delhi"}

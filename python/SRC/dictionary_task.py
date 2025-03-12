e9c5527b7fb6c6c0d2ed3da0f9a169abe3ca2841
taif-k <taifk817@gmail.com>
2025-03-12 12:31:24 +0530

    Add comment

---
+++ python/SRC/dictionary_task.py
@@ -0,0 +1,5 @@
+
+info = {"id":123, "name":"taif", "state":"delhi", "contact": 1234567890}  # dictionary
+
+data = f""" Id is: {info["id"]}\n Name is: {info["name"]}\n State is: {info["state"]}\n Contact is: {info["contact"]} """ # doc string
+print(data)

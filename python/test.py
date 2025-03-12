9a2d58caf4c93c1f14f656305369245b21eec53c
taif-k <taifk817@gmail.com>
2025-03-12 10:18:48 +0530

    cant print 0123 as int

---
+++ python/test.py
@@ -0,0 +1,18 @@
+# digit = 123
+# print(digit)
+
+number = 0123
+print(number)
+
+# digit = 0000
+# print(digit)
+
+# number = str('0123')
+# print(number)
+
+# number = str('0123')
+# print(int(number))
+
+# digit = int(0000)
+# print(digit)
+

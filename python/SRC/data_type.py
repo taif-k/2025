2a22f3376392ad56b7c8d2f58ca626ae97efa209
taif-k <taifk817@gmail.com>
2025-03-12 09:26:48 +0530

    rename and add new file

---
+++ python/SRC/data_type.py
@@ -0,0 +1,9 @@
+# doc string
+name = "taif"
+country = "India"
+info = f"""Hi {name}, I am using doc string for practise """
+print(info)
+
+# dictionary
+location = {"city":"New Delhi"}
+print(f"{country},",location["city"])

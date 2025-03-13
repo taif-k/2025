42a2baf26126a89a7a4e769a506d207e004bb80f
taif-k <taifk817@gmail.com>
2025-03-13 11:10:32 +0530

    test

--- python/test.py
+++ python/test.py
@@ -1,33 +1,14 @@
-# digit = 123
-# print(digit)
-# print(type(digit))
 
+check1 = "0123"
+print("\n ",type(check1),check1)
+check1 =  int(check1)
+print("\n ",type(check1),check1)
 
-# digit = 0000
-# print(digit)
-# print(type(digit))
-
-
-# number = str('0123')
-# print(number)
-# print(type(number))
-
-# number = str('0123')
-# print(int(number))
-# print(type(number))
-
-# digit = int(0000)
-# print(digit)
-# print(type(digit))
-
-number = "0123"
-print("\n1",type(number),number)
-number =  int(number)
-print("\n2",type(number),number)
+check2 = 0000
+print("\n ",type(check2),check2)
+check2 = str(check2)
+print("\n ",type(check2),check2)
 
 
 
-# number = "0123"
-# print(number)
-# print(type(number))
 

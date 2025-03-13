7a0d344779b3ba4c2bf57e8de0c43c8987e63c42
taif-k <taifk817@gmail.com>
2025-03-13 11:28:55 +0530

    string conversion to int

--- python/SRC/int_str_convert.py
+++ python/SRC/int_str_convert.py
@@ -1,14 +1,21 @@
 
 check1 = "0123"
+print("\nCheck 1")
 print("\n ",type(check1),check1)
 check1 =  int(check1)
 print("\n ",type(check1),check1)
 
 check2 = 0000
+print("\nCheck 2")
 print("\n ",type(check2),check2)
 check2 = str(check2)
 print("\n ",type(check2),check2)
 
+check3 = "0000"
+print("\nCheck 3")
+print("\n ",type(check3),check3)
+check3 = int(check3)
+print("\n ",type(check3),check3)
 
 
 

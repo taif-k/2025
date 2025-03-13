1339a7066216759957c81f08bcb0f51914d461fc
taif-k <taifk817@gmail.com>
2025-03-13 11:03:03 +0530

    test

--- python/test.py
+++ python/test.py
@@ -1,18 +1,33 @@
 # digit = 123
 # print(digit)
+# print(type(digit))
+
 
 # digit = 0000
 # print(digit)
+# print(type(digit))
+
 
 # number = str('0123')
 # print(number)
+# print(type(number))
 
 # number = str('0123')
 # print(int(number))
+# print(type(number))
 
 # digit = int(0000)
 # print(digit)
+# print(type(digit))
 
-number = 0123
-print(number)
+number = "0123"
+print("\n1",type(number),number)
+number =  int(number)
+print("\n2",type(number),number)
+
+
+
+# number = "0123"
+# print(number)
+# print(type(number))
 

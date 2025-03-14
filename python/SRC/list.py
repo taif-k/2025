bdf056446400ad6ecf14bfa288575f7cd96d7173
taif-k <taifk817@gmail.com>
2025-03-14 07:18:45 +0530

    pass list in set NO, pass list[index] in set OK

---
+++ python/SRC/list.py
@@ -0,0 +1,13 @@
+
+d1 = {1,2,3}
+d2 = {9,9,9,"t"}
+
+listdata = [d1,d2,9,9,"9"]
+# print(listdata) # we get d2 -> {9, t }
+
+
+list2 = [8,9]
+set3 = {list2[1],9}  # there will be type error is we pass list in set, but we can pass list2[index] in set
+print(set3)
+
+# list can have duplicate data and it is in ordered(sequence), it is in sequence so we can use index 

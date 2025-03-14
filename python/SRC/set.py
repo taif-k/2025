6d12bf717dd64c9e973449112253f5bf21e3d202
taif-k <taifk817@gmail.com>
2025-03-14 06:57:04 +0530

    use dict in set

--- python/SRC/set.py
+++ python/SRC/set.py
@@ -1,8 +1,7 @@
 data = {"id":132, "name": "taif"} 
 flag = "9" 
 
-setdata = {str(9),flag,9,data["name"],data["name"]}
-
+setdata = {str(9),flag,9,data["name"],data["name"],tuple(data)} #we cannot directly use data dictionary in setdata set because it is unhashable, use tuple to aviod error
 print(setdata)
 
 

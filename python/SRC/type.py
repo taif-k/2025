406cfffe2e6e7cacd8a00c69dc48f254153b403b
taif-k <taifk817@gmail.com>
2025-03-14 08:21:44 +0530

    add comment

---
+++ python/SRC/type.py
@@ -0,0 +1,15 @@
+#  type(variable_name) tells the datatype, check for 0, 0000 , 0123, 1, "1", "0" etc
+
+
+
+num = "0"
+print(type(num),num)
+
+num = 0
+print(type(num),num)
+
+num = 0000
+print(type(num),num)
+
+num = "1"
+print(type(num),num)

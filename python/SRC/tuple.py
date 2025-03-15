b226b1d82a595f23beff9ba7d6ad4a0fe1adfc1f
taif-k <taifk817@gmail.com>
2025-03-15 12:12:11 +0530

    pass list and set in tuple

--- python/SRC/tuple.py
+++ python/SRC/tuple.py
@@ -1,11 +1,14 @@
 
 # tupledata[0] = 5 'tuple' object does not support item assignment, we generally keep that data here which is not changed
-setdata = {1,2,3}
-tupledata = (1,setdata)
+setdata = {"set",1,2}
+list = [3,"list"]
+tupledata = (1,setdata,list)
 # tupledata = (5,5,"five") # but we can completely update the tuple but not the particularly indexing
 print(tupledata)
 
 
 
 # [{}] OK
+# ({}, [], ) Ok
+
 # {[]} NO 

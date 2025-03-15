73753342456a4693df5bf072d78ea603fd92b689
taif-k <taifk817@gmail.com>
2025-03-15 11:08:25 +0530

    add comment

--- python/SRC/list.py
+++ python/SRC/list.py
@@ -1,14 +1,17 @@
 
 d1 = {1,2,3}
 d2 = {9,9,9,"t"}
-
 listdata = [d1,d2,9,9,"9"]
-# print(listdata) # we get d2 -> {9, t }
+# print(listdata) 
 
+# # only takes one arg, here we have removed d2, anything can be removed from anywhere in the list
+print(listdata) 
+listdata.pop(1) # pop takes index
+print(listdata) 
 
-list2 = [8,9]
-set3 = {list2[1],9}  # there will be type error is we pass list in set, but we can pass list2[index] in set
-print(set3)
+# list2 = [8,9]
+# set3 = {list2[1],9}  # there will be type error is we pass list in set, but we can pass list2[index] in set
+# # print(set3)
 
 # list can have duplicate data and it is in ordered(sequence), it is in sequence so we can use index 
 

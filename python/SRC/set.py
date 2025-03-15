87c9a824c3fd39e0906b4f638e17fb5f691f7d74
taif-k <taifk817@gmail.com>
2025-03-15 10:26:35 +0530

    print set

--- python/SRC/set.py
+++ python/SRC/set.py
@@ -2,7 +2,7 @@ data = {"id":132, "name": "taif"}
 flag = "9" 
 
 setdata = {str(9),flag,9,data["name"],data["name"],tuple(data)} #we cannot directly use data dictionary in setdata set because it is unhashable, use tuple to aviod error
-# print(setdata)
+print(setdata)
 
 
 # Set is collection of unique unordered data 

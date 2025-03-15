05ae960bc5574a724d74e48af6c9f0cd79017003
taif-k <taifk817@gmail.com>
2025-03-15 09:11:57 +0530

    comment

--- python/SRC/student.py
+++ python/SRC/student.py
@@ -1,11 +1,16 @@
+
 id = int(input("Enter id: "))
 name = input("Enter name: ")
 address = input("Enter address: ")
 contact = int(input("Enter contact: "))
 qualification = input("Enter qualification: ")
 
-
 studentdetails = {"id":id,"name":name,"address":address,"contact":contact,"qualification":qualification}
-studentstr = f"""id: {studentdetails["id"]} \nname: {studentdetails["name"]} \naddress: {studentdetails["address"]} \ncontact: {studentdetails["contact"] } \nqualification: {studentdetails["qualification"]}"""
-print("\n---student details---")
-print(studentstr)
+# studentstr = f"""id: {studentdetails["id"]} \nname: {studentdetails["name"]} \naddress: {studentdetails["address"]} \ncontact: {studentdetails["contact"] } \nqualification: {studentdetails["qualification"]}"""   
+print(studentdetails)
+
+
+
+
+
+

55ac5856ae52adc3cbefb72f2a09f6a4755db391
taif-k <taifk817@gmail.com>
2025-03-19 12:29:03 +0530

    nested dict

--- python/SRC/student.py
+++ python/SRC/student.py
@@ -1,16 +1,37 @@
 import json
-studentdetails = {} 
+students = {} 
+qualificationdict = {}
+listdata = []
 
-studentdetails["id"] = (input("Enter id: "))
-studentdetails["name"] = input("Enter name: ")
-studentdetails["address"] = input("Enter address: ")
-studentdetails["contact"] = int(input("Enter contact: "))
-studentdetails["contact"] = input("Enter qualification: ")
+students["id"] = int(input("Enter id: "))
+students["name"] = input("Enter name: ")
+students["address"] = input("Enter address: ")
+students["contact"] = int(input("Enter contact: "))
 
-# studentstr = f"""id: {studentdetails["id"]} \nname: {studentdetails["name"]} \naddress: {studentdetails["address"]} \ncontact: {studentdetails["contact"] } \nqualification: {studentdetails["qualification"]}"""   
-print(json.dumps(studentdetails,indent=4))
-# print(studentstr)
+
+qualificationdict["qulaificationname"] = "12th"
+qualificationdict["passingyear"] = "2016"
+listdata.append(qualificationdict)
+
+qualificationdict["qulaificationname"] = "bca"
+qualificationdict["passingyear"] = "2020"
+listdata.append(qualificationdict)
+
+qualificationdict["qulaificationname"] = "mca"
+qualificationdict["passingyear"] = "2024"
+listdata.append(qualificationdict)
+
+students["qualifications"] = listdata
+
+
+
+
+# # studentstr = f"""id: {studentdetails["id"]} \nname: {studentdetails["name"]} \naddress: {studentdetails["address"]} \ncontact: {studentdetails["contact"] } \nqualification: {studentdetails["qualification"]}"""   
+# print(json.dumps(studentdetails,indent=4))
+print(students)
+# print(json.dumps(students,indent=4))
  
+
 
 
 

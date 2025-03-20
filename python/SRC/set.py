db87c92f82cf2cf7cde8536625de7c05135af5d6
taif-k <taifk817@gmail.com>
2025-03-20 08:00:57 +0530

    use remove function

--- python/SRC/set.py
+++ python/SRC/set.py
@@ -1,17 +1,27 @@
-data = {"id":132, "name": "taif"} 
-flag = "9" 
+# data = {"id":132, "name": "taif"} 
+# flag = "9" 
 
-setdata = {str(9),flag,9,data["name"],data["name"],tuple(data)} #we cannot directly use data dictionary in setdata set because it is unhashable, use tuple to aviod error
-print(setdata)
-
-
-# Set is collection of unique unordered data 
-
-# [{}] OK
-# {[]} NO
+# setdata = {str(9),flag,9,data["name"],data["name"],tuple(data)} #we cannot directly use data dictionary in setdata set because it is unhashable, use tuple to aviod error
+# print(setdata)
 
 
-# s1 = {1,2,3}
-# s2 = {2,4,5}
-# s3 = {s1,s2}
-# print(s3) # {{}} NO
+# # Set is collection of unique unordered data 
+
+# # [{}] OK
+# # {[]} NO
+
+
+# # s1 = {1,2,3}
+# # s2 = {2,4,5}
+# # s3 = {s1,s2}
+# # print(s3) # {{}} NO
+
+
+setdata1 = {1,2,3,4,5}
+# setdata2 = {3,4,5,6}
+
+# setdata.remove(2)
+# print(setdata2.difference(setdata1))
+
+# setdata.remove(3)
+print(setdata1)

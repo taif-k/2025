0027fa109d7450994006ce7220289470fffb4fb6
taif-k <taifk817@gmail.com>
2025-03-20 14:42:12 +0530

    upadate comment

--- python/SRC/tuple.py
+++ python/SRC/tuple.py
@@ -1,11 +1,18 @@
 
 # tupledata[0] = 5 'tuple' object does not support item assignment, we generally keep that data here which is not changed
-setdata = {"set",1,2}
-list = [3,"list"]
-tupledata = (1,setdata,list)
-# tupledata = (5,5,"five") # but we can completely update the tuple but not the particularly indexing
-print(tupledata)
+# setdata = {1,2}
+# setdata2 = {1,2}
+# list = [2,"list"]
+# tupledata = (1,setdata,setdata2,list)
+# # tupledata = (5,5,"five") # but we can completely update the tuple but not the particularly indexing
+# # print(tupledata.index())
 
+
+
+# tuple2 = ((1,2))
+# print(tupledata)
+# data1 = {10,20,30}
+# print(data1)
 
 
 # [{}] OK

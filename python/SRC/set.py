9cb2d8cb791b11bbc21dc7b07da971aa077e33fd
taif-k <taifk817@gmail.com>
2025-03-20 11:57:50 +0530

    use add,remove,pop and union

--- python/SRC/set.py
+++ python/SRC/set.py
@@ -18,9 +18,12 @@
 
 
 setdata1 = {1,2,3,4}
-# setdata1.add(6-1)
-# setdata1.discard("string") # discard will not throw an error unlike remove, if the element is not present in set 
-# setdata1.remove(2)
+setdata2 = {3,4,5,"string"}
+setdata1.add(6-1)
+setdata1.discard("string") # discard will not throw an error unlike remove, if the element is not present in set 
+setdata1.remove(2) # 1 3 4 5
 setdata1.pop() # when using pop in set it does not take any arg(index was passed as arg in list) but set is unordered so no index, pop in set will remove randomly
+# 3 4 5 
+print(setdata1.union(setdata2)) # here two sets are combined and unique elements are printed
 
-print(setdata1)
+# print(setdata1)

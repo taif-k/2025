4ad12f7364911e923a4789ea91b1c588d59c8067
taif-k <taifk817@gmail.com>
2025-03-20 14:39:00 +0530

    update variable

--- python/SRC/two_students_registration.py
+++ python/SRC/two_students_registration.py
@@ -29,25 +29,25 @@ studentqualification["passingyear"] = int(input("Enter passing year: "))
 qualificationlist.append(studentqualification)
 #//////////////////////////////////////////////////////////////////////////////
 studentdict2 = {}
-studentqualification = {}
-qualificationlist = []
+studentqualification2 = {}
+qualificationlist2 = []
 
-studentskills = []
+studentskills2 = []
 
 studentdict2["studentid"] = int(input("Enter id: "))
 studentdict2["studentname"] = input("Enter name: ")
 studentdict2["experience"] = input("Enter experience: ")
-studentdict2["skills"] = studentskills
-studentdict2["Qualification"] = qualificationlist
+studentdict2["skills"] = studentskills2
+studentdict2["Qualification"] = qualificationlist2
 
-studentskills.append(input("Enter 1st skill: "))
-studentskills.append(input("Enter 2nd skill: "))
-studentskills.append(input("Enter 3rd skill: "))
+studentskills2.append(input("Enter 1st skill: "))
+studentskills2.append(input("Enter 2nd skill: "))
+studentskills2.append(input("Enter 3rd skill: "))
 
-studentqualification["name"] = input("Enter qualification: ")
-studentqualification["passingyear"] = input("Enter passing year: ")
-qualificationlist.append(studentqualification) #
+studentqualification2["name"] = input("Enter qualification: ")
+studentqualification2["passingyear"] = input("Enter passing year: ")
+qualificationlist2.append(studentqualification2) #
 
-mainlist = [studentdict,studentdict]
+mainlist = [studentdict,studentdict2]
 print(json.dumps(mainlist,indent=4))
 

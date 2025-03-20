476691d2ca91628093b3fe6fab96fba36caede7c
taif-k <taifk817@gmail.com>
2025-03-20 08:03:23 +0530

    print multiple qualification

--- python/SRC/student.py
+++ python/SRC/student.py
@@ -1,7 +1,6 @@
-import json
+
 students = {} 
-qualificationdict = {}
-listdata = []
+
 
 students["id"] = int(input("Enter id: "))
 students["name"] = input("Enter name: ")
@@ -9,27 +8,9 @@ students["address"] = input("Enter address: ")
 students["contact"] = int(input("Enter contact: "))
 
 
-qualificationdict["qulaificationname"] = "12th"
-qualificationdict["passingyear"] = "2016"
-listdata.append(qualificationdict)
-
-qualificationdict["qulaificationname"] = "bca"
-qualificationdict["passingyear"] = "2020"
-listdata.append(qualificationdict)
-
-qualificationdict["qulaificationname"] = "mca"
-qualificationdict["passingyear"] = "2024"
-listdata.append(qualificationdict)
-
-students["qualifications"] = listdata
-
-
-
-
-# # studentstr = f"""id: {studentdetails["id"]} \nname: {studentdetails["name"]} \naddress: {studentdetails["address"]} \ncontact: {studentdetails["contact"] } \nqualification: {studentdetails["qualification"]}"""   
-# print(json.dumps(studentdetails,indent=4))
 print(students)
-# print(json.dumps(students,indent=4))
+
+
  
 
 

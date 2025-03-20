9f60240239e1494acf2b7edc4a8c37a445d4d3eb
taif-k <taifk817@gmail.com>
2025-03-20 09:00:21 +0530

    print qualification without list, change key names

--- python/SRC/student.py
+++ python/SRC/student.py
@@ -1,12 +1,23 @@
 
 students = {} 
-
+listdata = []
+qualificationdata = {}
 
 students["id"] = int(input("Enter id: "))
 students["name"] = input("Enter name: ")
 students["address"] = input("Enter address: ")
 students["contact"] = int(input("Enter contact: "))
+students["Qualification"] = qualificationdata 
 
+
+qualificationdata["qualification"] = input("Enter 1st qualification: ")
+qualificationdata["passingyear"] = int(input("Enter 1st qualification passing year: "))
+# listdata.append(quliaficationdata)
+
+# quliaficationdata = {}
+qualificationdata["qualification2"] = input("Enter 2st qualification: ")
+qualificationdata["passingyear2"] = int(input("Enter 2st qualification passing year: "))
+# listdata.append(quliaficationdata)
 
 print(students)
 

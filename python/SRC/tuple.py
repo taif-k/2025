21fe39d0db7d26a8010432e0e23abf679b1fff66
taif-k <taifk817@gmail.com>
2025-03-21 09:41:00 +0530

    practice

--- python/SRC/tuple.py
+++ python/SRC/tuple.py
@@ -1,15 +1,18 @@
 
 # tupledata[0] = 5 'tuple' object does not support item assignment, we generally keep that data here which is not changed
 setdata = {1,2}
-setdata2 = {1,2}
+setdata2 = (91,20)
 list = [2,"list"]
-tupledata1 = (1,setdata,setdata2,list)
-print(tupledata1)
+tuple1 = (1,setdata,setdata2,list)
+print(tuple1)
 
 tuple2 = ((9,8))
 print(tuple2[0])
 print(tuple2[1])
 
+data3, data4 = (10,"20")
+print(type(data3),data3)
+print(type(data4),data4)
 
 
 # [{}] OK

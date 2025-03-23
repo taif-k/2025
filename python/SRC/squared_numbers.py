fc586451e54215ab11e64b7b238b3144bfa2d0b3
taif-k <taifk817@gmail.com>
2025-03-23 16:35:58 +0530

    remove comments

--- python/SRC/squared_numbers.py
+++ python/SRC/squared_numbers.py
@@ -1,6 +1,6 @@
-numbers =list(range(1,11))  # generating numbers 1 t0 10 in a list using range
-numbers = numbers[:5]       #  using slice to get first 5 numbers
+numbers =list(range(1,11))  
+numbers = numbers[:5]      
 
 squared_numbers = {num ** 2 for num in numbers }  # using for loop to iterate over each num from 1 - 5 and squaring it using ( ** 2)
-                                                  # also keeping squared numbers in a set  
+                                                   
 print(squared_numbers)

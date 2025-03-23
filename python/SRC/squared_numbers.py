3485f5fcda3f669bc376429fccea3688933de9cd
taif-k <taifk817@gmail.com>
2025-03-23 17:18:50 +0530

    square of descending no's

--- python/SRC/squared_numbers.py
+++ python/SRC/squared_numbers.py
@@ -1,5 +1,7 @@
 numbers = list(range(10,0,-1))
-print(numbers)
+print("Numbers: ",numbers)
+numbers = numbers[:6]
+print("First 5 numbers: ",numbers)
 
 squared_numbers = [num ** 2 for num in numbers]
-print(squared_numbers)
+print("Square of first 5 descending numbers: ",squared_numbers)

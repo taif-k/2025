9e7d23b0c51d14889d5404c2c2488302ae31a12a
taif-k <taifk817@gmail.com>
2025-03-24 07:51:29 +0530

    remove print

--- python/SRC/dictionary.py
+++ python/SRC/dictionary.py
@@ -3,4 +3,3 @@ info = {"id":123, "fname":"taif", "state":state[0], "contact": 1234567890}
 
 data = f""" Id is: {info["id"]}\n Name is: {info["fname"]}\n State is: {info["state"]}\n Contact is: {info["contact"]} """
 print(data)
-print("\n",state)

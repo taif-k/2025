05ab33ea41e72151d5f2736b6883128a9bb119ad
taif-k <taifk817@gmail.com>
2025-03-24 10:36:54 +0530

    remove comments

--- python/SRC/set.py
+++ python/SRC/set.py
@@ -1,29 +1,11 @@
-# data = {"id":132, "name": "taif"} 
-# flag = "9" 
-
-# setdata = {str(9),flag,9,data["name"],data["name"],tuple(data)} #we cannot directly use data dictionary in setdata set because it is unhashable, use tuple to aviod error
-# print(setdata)
-
-
-# # Set is collection of unique unordered data 
-
-# # [{}] OK
-# # {[]} NO
-
-
-# # s1 = {1,2,3}
-# # s2 = {2,4,5}
-# # s3 = {s1,s2}
-# # print(s3) # {{}} NO
-
+data = {"id":132, "name": "taif"} 
+set3 = {tuple(data.values())} 
+print("dictionary is printed inside set using tuple: ",set3)
 
 setdata1 = {1,2,3,4}
-setdata2 = {3,4,5,"string"}
-setdata1.add(6-1)
+setdata2 = {4,5,6}
+setdata1.add(7)
 setdata1.discard("string") # discard will not throw an error unlike remove, if the element is not present in set 
-setdata1.remove(2) # 1 3 4 5
+setdata1.remove(2) 
 setdata1.pop() # when using pop in set it does not take any arg(index was passed as arg in list) but set is unordered so no index, pop in set will remove randomly
-# 3 4 5 
-print(setdata1.union(setdata2)) # here two sets are combined and unique elements are printed
-
-# print(setdata1)
+print(setdata1)

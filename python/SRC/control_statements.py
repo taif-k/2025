99c123e9f444c3e04e52503ac6b5ee99b0181c05
taif-k <taifk817@gmail.com>
2025-03-24 14:24:31 +0530

    numbers less & greater than 5

---
+++ python/SRC/control_statements.py
@@ -0,0 +1,90 @@
+listdata = [1,2,3,4,5,6,7,8,9,10]
+smalllist = []
+largelist = []
+
+
+if listdata[0] < 5:
+    smalllist.append(listdata[0])
+else:
+    largelist.append(listdata[0])
+
+if listdata[1] < 5:
+    smalllist.append(listdata[1])
+else:
+    largelist.append(listdata[1])
+
+if listdata[2] < 5:
+    smalllist.append(listdata[2])
+else:
+    largelist.append(listdata[2])
+
+if listdata[3] < 5:
+    smalllist.append(listdata[3])
+else:
+    largelist.append(listdata[3])
+
+if listdata[4] <= 5:
+    smalllist.append(listdata[4])
+else:
+    largelist.append(listdata[4])
+
+if listdata[5] < 5:
+    smalllist.append(listdata[5])
+else:
+    largelist.append(listdata[5])                     
+
+if listdata[6] < 5:
+    smalllist.append(listdata[6])
+else:
+    largelist.append(listdata[6])
+
+if listdata[7] < 5:
+    smalllist.append(listdata[7])
+else:
+    largelist.append(listdata[7])
+
+if listdata[8] < 5:
+    smalllist.append(listdata[8])
+else:
+    largelist.append(listdata[8])
+
+if listdata[9] < 5:
+    smalllist.append(listdata[9])
+else:
+    largelist.append(listdata[9])
+
+print("Less than 5: ",smalllist)
+print("Greater than 5: ",largelist)                        
+
+# if numbers[:5]:
+#     print("Less than 5: ",numbers[:4])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# if numbers[0] < 5:
+#     print("Less than 5")
+# elif numbers[1] < 5:
+#     print("less than 5")
+# elif numbers[2] < 5:
+#     print("less than 5")
+# elif numbers[3] < 5:
+#     print("less than 5")
+# elif numbers[4] < 5:
+#     print("less than 5")
+# elif numbers[5] < 5:
+#     print("less than 5")             

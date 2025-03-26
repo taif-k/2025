742d44a407fe2c8ff793687ef01dc5c5a174fe2f
taif-k <taifk817@gmail.com>
2025-03-26 07:19:31 +0530

    useing while loop to keep asking until correct number is entered

---
+++ python/SRC/random_number.py
@@ -0,0 +1,39 @@
+import random
+
+random_number = random.randint(0,9)
+
+while True:
+        user_number = input("Enter number: ")
+        if user_number.isdigit():
+             user_number = int(user_number)
+             if user_number <0 or user_number >9:
+                print("please enter number 0 - 9")
+             else:
+                  if user_number == random_number:
+                       print("you're the winner")
+                       print("Generated number: ",random_number)
+                       break
+                  else:
+                       print("Indixpert won")
+                       print("Generated number: ",random_number)
+                       break
+        else:
+            print("Enter only Numbers")    
+
+    
+        
+        
+        
+            
+                
+            
+                
+
+
+
+    
+
+
+
+
+

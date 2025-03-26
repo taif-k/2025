d20dad59e05b72729d366533059a528d7a924ea2
taif-k <taifk817@gmail.com>
2025-03-26 14:33:17 +0530

    update

--- python/SRC/salary.py
+++ python/SRC/salary.py
@@ -1,6 +1,5 @@
 salaries = []
 
-
 salary = input("Enter salary 1: ")
 if salary.isdigit():
         salary = int(salary)
@@ -12,7 +11,6 @@ if salary.isdigit():
 else:
     print("Enter salary in Positive numbers Only")
 
-
 salary = input("Enter salary 2: ")
 if salary.isdigit():
     salary = int(salary)
@@ -23,7 +21,6 @@ if salary.isdigit():
 else:
     print("Enter salary in Positive numbers Only")
 
-
 salary = input("Enter salary 3: ")
 if salary.isdigit():
     salary = int(salary)
@@ -33,7 +30,6 @@ if salary.isdigit():
         print("Enter salary between 10,000 and 1,00,000")    
 else:
     print("Enter salary in Positive numbers Only")
-
 
 salary = input("Enter salary 4: ")
 if salary.isdigit():

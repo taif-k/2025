94fb3cba1b5cb73405b6d4f75181643ce0a3afdc
taif-k <taifk817@gmail.com>
2025-03-26 10:25:44 +0530

    added else stat

--- python/SRC/highest_salary.py
+++ python/SRC/highest_salary.py
@@ -3,23 +3,31 @@ salaries = []
 salary1 = int(input("Enter salary 1: "))
 if salary1 >=10000:
     salaries.append(salary1)  
+else:
+    print("Enter higher salary")
    
 
 salary2 = int(input("Enter salary 2: "))
 if salary2 >=10000:
     salaries.append(salary2)
+else:
+    print("Enter higher salary")
 
 salary3 = int(input("Enter salary 3: "))
 if salary3 >=10000:
     salaries.append(salary3)  
+else:
+    print("Enter higher salary")    
 
 salary4 = int(input("Enter salary 4: "))
 if salary4 >=10000:
-    salaries.append(salary4)      
+    salaries.append(salary4)   
+else:
+    print("Enter higher salary")       
 
 
 salaries.sort()
-print(salaries[1:])    
+# print(salaries[1:])    
 
 
 

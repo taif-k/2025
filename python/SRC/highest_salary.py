345100fd04c63e13ff223187bd3353724f3e25ed
taif-k <taifk817@gmail.com>
2025-03-26 11:03:32 +0530

    keep asking for user input untill condition is matched

--- python/SRC/highest_salary.py
+++ python/SRC/highest_salary.py
@@ -1,33 +1,53 @@
 salaries = []
 
-salary1 = int(input("Enter salary 1: "))
-if salary1 >=10000:
-    salaries.append(salary1)  
-else:
-    print("Enter higher salary")
-   
+while True:
+    salary1 = input("Enter salary 1: ")
+    if salary1.isdigit():
+        salary1 = int(salary1)
+        if salary1 >=10000:
+            salaries.append(salary1) 
+            break 
+        else:
+            print("Enter more than or equal to 10,000")
+    else:
+        print("Enter salary in Digits Only")
 
-salary2 = int(input("Enter salary 2: "))
-if salary2 >=10000:
-    salaries.append(salary2)
-else:
-    print("Enter higher salary")
+while True:
+    salary2 = input("Enter salary 2: ")
+    if salary2.isdigit():
+        salary2 = int(salary2)
+        if salary2 >=10000:
+            salaries.append(salary2)
+            break
+        else:
+            print("Enter more than or equal to 10,000")
+    else:
+        print("Enter salary in Digits Only")
 
-salary3 = int(input("Enter salary 3: "))
-if salary3 >=10000:
-    salaries.append(salary3)  
-else:
-    print("Enter higher salary")    
+while True:
+    salary3 = input("Enter salary 3: ")
+    if salary3.isdigit():
+        salary3 = int(salary3)
+        if salary3 >=10000:
+            salaries.append(salary3)
+            break  
+        else:
+            print("Enter more than or equal to 10,000")    
+    else:
+        print("Enter salary in Digits Only")
 
-salary4 = int(input("Enter salary 4: "))
-if salary4 >=10000:
-    salaries.append(salary4)   
-else:
-    print("Enter higher salary")       
+while True:
+    salary4 = input("Enter salary 4: ")
+    if salary4.isdigit():
+        salary4 = int(salary4)
+        if salary4 >=10000:
+            salaries.append(salary4)
+            break   
+        else:
+            print("Enter higher salary")       
 
 salaries.sort()
-print(salaries)
-# print(salaries[1:])    
+print("Two Highest salaries are ",salaries[2:])    
 
 
 

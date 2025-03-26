0d5b338a3ec11b38338fdda873898a38bceb7671
taif-k <taifk817@gmail.com>
2025-03-26 07:33:46 +0530

    changed random() range

--- python/SRC/random_number.py
+++ python/SRC/random_number.py
@@ -1,13 +1,13 @@
 import random
 
-random_number = random.randint(0,9)
+random_number = random.randint(1,20)
 
 while True:
         user_number = input("Enter number: ")
         if user_number.isdigit():
              user_number = int(user_number)
-             if user_number <0 or user_number >9:
-                print("please enter number 0 - 9")
+             if user_number <1 or user_number >20:
+                print("please enter number 1 - 20")
              else:
                   if user_number == random_number:
                        print("you're the winner")

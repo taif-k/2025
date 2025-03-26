76b3b6b35cfe536cb6d39718537bca6fc4a489cb
taif-k <taifk817@gmail.com>
2025-03-26 14:24:25 +0530

    rename variable

--- python/SRC/salary.py
+++ python/SRC/salary.py
@@ -1,11 +1,11 @@
 salaries = []
 
 
-salary1 = input("Enter salary 1: ")
-if salary1.isdigit():
-        salary1 = int(salary1)
-        if salary1 >=10000:
-            salaries.append(salary1) 
+salary = input("Enter salary 1: ")
+if salary.isdigit():
+        salary = int(salary)
+        if salary >=10000:
+            salaries.append(salary) 
             
         else:
             print("Enter more than or equal to 10,000")
@@ -13,33 +13,33 @@ else:
     print("Enter salary in Digits Only")
 
 
-salary2 = input("Enter salary 2: ")
-if salary2.isdigit():
-    salary2 = int(salary2)
-    if salary2 >=10000:
-        salaries.append(salary2)    
+salary = input("Enter salary 2: ")
+if salary.isdigit():
+    salary = int(salary)
+    if salary >=10000:
+        salaries.append(salary)    
     else:
         print("Enter more than or equal to 10,000")
 else:
     print("Enter salary in Digits Only")
 
 
-salary3 = input("Enter salary 3: ")
-if salary3.isdigit():
-    salary3 = int(salary3)
-    if salary3 >=10000:
-            salaries.append(salary3)
+salary = input("Enter salary 3: ")
+if salary.isdigit():
+    salary = int(salary)
+    if salary >=10000:
+            salaries.append(salary)
     else:
         print("Enter more than or equal to 10,000")    
 else:
     print("Enter salary in Digits Only")
 
 
-salary4 = input("Enter salary 4: ")
-if salary4.isdigit():
-    salary4 = int(salary4)
-    if salary4 >=10000:
-        salaries.append(salary4)       
+salary = input("Enter salary 4: ")
+if salary.isdigit():
+    salary = int(salary)
+    if salary >=10000:
+        salaries.append(salary)       
     else:
         print("Enter higher salary")       
 

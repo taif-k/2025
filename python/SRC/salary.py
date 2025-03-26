5fb92ba59fa5b683906e226a9393c536af32e469
taif-k <taifk817@gmail.com>
2025-03-26 14:27:51 +0530

    hello

--- python/SRC/salary.py
+++ python/SRC/salary.py
@@ -4,41 +4,41 @@ salaries = []
 salary = input("Enter salary 1: ")
 if salary.isdigit():
         salary = int(salary)
-        if salary >=10000:
+        if salary >=10000 and salary <=100000:
             salaries.append(salary) 
             
         else:
-            print("Enter more than or equal to 10,000")
+            print("Enter salary between 10,000 and 1,00,000")
 else:
-    print("Enter salary in Digits Only")
+    print("Enter salary in Positive numbers Only")
 
 
 salary = input("Enter salary 2: ")
 if salary.isdigit():
     salary = int(salary)
-    if salary >=10000:
+    if salary >=10000 and salary <=100000:
         salaries.append(salary)    
     else:
-        print("Enter more than or equal to 10,000")
+        print("Enter salary between 10,000 and 1,00,000")
 else:
-    print("Enter salary in Digits Only")
+    print("Enter salary in Positive numbers Only")
 
 
 salary = input("Enter salary 3: ")
 if salary.isdigit():
     salary = int(salary)
-    if salary >=10000:
+    if salary >=10000 and salary <=100000:
             salaries.append(salary)
     else:
-        print("Enter more than or equal to 10,000")    
+        print("Enter salary between 10,000 and 1,00,000")    
 else:
-    print("Enter salary in Digits Only")
+    print("Enter salary in Positive numbers Only")
 
 
 salary = input("Enter salary 4: ")
 if salary.isdigit():
     salary = int(salary)
-    if salary >=10000:
+    if salary >=10000 and salary <=100000:
         salaries.append(salary)       
     else:
         print("Enter higher salary")       

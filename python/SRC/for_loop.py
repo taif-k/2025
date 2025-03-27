e41ab98cdf10eace0f02c7ec4b7e19dc7a689de8
taif-k <taifk817@gmail.com>
2025-03-27 09:16:19 +0530

    find name in a list

--- python/SRC/for_loop.py
+++ python/SRC/for_loop.py
@@ -1,8 +1,8 @@
 student = {}
 
-student["id"] = int(input("Enter id: "))
-student["name"] = input("Enter name: ")
-student["contact"] = int(input("Enter contact: "))
+# student["id"] = int(input("Enter id: "))
+# student["name"] = input("Enter name: ")
+# student["contact"] = int(input("Enter contact: "))
 
 for keys,values in student.items():  # items is used here for both keys and values, for only keys or values use student.keys or student.values respectively.  
     print(f"""{keys}:{values}""")
@@ -10,3 +10,18 @@ for keys,values in student.items():  # items is used here for both keys and valu
 for keys in student.items(): 
     print(f"""{keys}""") # iterating over car.items(), but unpacking the key-value tuple, means key will hold the entire tuple (key, value).  
                          # use student.keys to het values of keys
+
+
+namelist = ["taif","sumit","srishti","sheetal"]
+
+not_present = True
+
+for name in namelist:
+    if name == "taif":
+        print(f"{name} is present")
+        break
+    else:
+        not_present = False
+
+if not_present == False:
+    print("Not present")        

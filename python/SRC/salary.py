e67d3e46bb4583ad071f1a91742456b64c9d9a55
taif-k <taifk817@gmail.com>
2025-03-27 12:35:16 +0530

    add dict in list

--- python/SRC/salary.py
+++ python/SRC/salary.py
@@ -4,14 +4,17 @@ for salary in range(0,4):
     sal = (input("Enter salary: "))
     if sal.isdigit():
         sal = int(sal)
-        if sal >=10000: 
+        if sal >=10000:
+            dict = {}
+            dict["id"] = int(input("Enter id: "))
+            salaries.append(dict) 
             salaries.append(sal)
         else:
             print("Salary should be minimum 10,000")
     else:
         print("Salary should be in digits")        
 
-salaries.sort(reverse=True)    
-print("salary in descending order",salaries)
+
+print(salaries)
     
 

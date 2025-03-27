19247f9811b513c73684abca6f01805a2f0b20b0
taif-k <taifk817@gmail.com>
2025-03-27 10:12:37 +0530

    replaced multiple if with for loop

--- python/SRC/salary.py
+++ python/SRC/salary.py
@@ -1,49 +1,17 @@
 salaries = []
 
-salary = input("Enter salary 1: ")
-if salary.isdigit():
-        salary = int(salary)
-        if salary >=10000 and salary <=100000:
-            salaries.append(salary) 
-            
+for salary in range(0,4):
+    sal = (input("Enter salary: "))
+    if sal.isdigit():
+        sal = int(sal)
+        if sal >=10000: 
+            salaries.append(sal)
         else:
-            print("Enter salary between 10,000 and 1,00,000")
-else:
-    print("Enter salary in Positive numbers Only")
-
-salary = input("Enter salary 2: ")
-if salary.isdigit():
-    salary = int(salary)
-    if salary >=10000 and salary <=100000:
-        salaries.append(salary)    
+            print("Salary should be minimum 10,000")
     else:
-        print("Enter salary between 10,000 and 1,00,000")
-else:
-    print("Enter salary in Positive numbers Only")
+        print("Salary should be in digits")        
 
-salary = input("Enter salary 3: ")
-if salary.isdigit():
-    salary = int(salary)
-    if salary >=10000 and salary <=100000:
-            salaries.append(salary)
-    else:
-        print("Enter salary between 10,000 and 1,00,000")    
-else:
-    print("Enter salary in Positive numbers Only")
-
-salary = input("Enter salary 4: ")
-if salary.isdigit():
-    salary = int(salary)
-    if salary >=10000 and salary <=100000:
-        salaries.append(salary)       
-    else:
-        print("Enter higher salary")       
-
-salaries.sort(reverse=True)
-print("Two Highest salaries are ",salaries[:2])    
-
-
-
-
-
+salaries.sort(reverse=True)    
+print("salary in descending order",salaries)
+    
 

8265333414b79f5a6900097ff6d3aa5ab1032928
taif-k <taifk817@gmail.com>
2025-03-29 09:17:31 +0530

    working on qualification search

--- python/SRC/dynamic_qualifications.py
+++ python/SRC/dynamic_qualifications.py
@@ -1,6 +1,7 @@
 import json
 
 listdata = []
+
 for num in range(1,4):
     studentdata = {}
     qualificationlist = []
@@ -9,7 +10,7 @@ for num in range(1,4):
     studentdata["qualification"] = qualificationlist
     for q in range(1,3):
         qualificationdict = {}
-        qualificationdict["qname"] = input("Enter qualification: ")
+        qualificationdict["qname"] =  input("Enter qualification: ")
         qualificationdict["passingyear"] = int(input("Enter passing year: "))
         qualificationlist.append(qualificationdict)
         ask_qualification = input("Add More qualification - y or n: ")
@@ -19,21 +20,16 @@ for num in range(1,4):
             break 
 
     listdata.append(studentdata)
-
+    
 print(json.dumps(listdata,indent=4))
-for x in range(1,10000000):
-    qualification_search = input("Enter qualification to search: ")
-    if qualification_search.isalpha():
-        for d in listdata:
-            for keys in d.keys():
-                if keys == "qualification":
-                    for n in qualificationlist:
-                        for value in n.values():
-                            if value == qualification_search:
-                                print(d["name"])
-        break
-    else:
-        print("Enter valid Qualification")
+
+
+
+
+
+       
+
+   
 
                 
 

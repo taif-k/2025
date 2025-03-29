549f819e878a7c83bb61ccaefe03d10e2f606da7
taif-k <taifk817@gmail.com>
2025-03-29 20:35:14 +0530

    have to make search menu

--- python/SRC/search_qualifications.py
+++ python/SRC/search_qualifications.py
@@ -1,18 +1,16 @@
 import json
-
 listdata = []
-
 for num in range(1,4):
     studentdata = {}
     qualificationlist = []
-    studentdata["id"] = input("Enter id: ")
+    studentdata["id"] =  num
     studentdata["name"] = input("Enter name: ")
     studentdata["qualification"] = qualificationlist
 
     while True:
         qualificationdict = {}
         qualificationdict["qname"] =  input("Enter qualification: ")
-        qualificationdict["passingyear"] = int(input("Enter passing year: "))
+        qualificationdict["passingyear"] = input("Enter passing year")
         qualificationlist.append(qualificationdict)
 
         ask_qualification = input("Add More qualification - y or n: ")
@@ -20,40 +18,27 @@ for num in range(1,4):
         if ask_qualification.lower() != "y":
             break
 
-
     listdata.append(studentdata)
     
 print(json.dumps(listdata,indent=4))
 
+while True:
+    qualification_search = input("Enter qualification to search: ")
+    if qualification_search.isalnum():
+        ispresent = False
+        for d in listdata:
+            for qualification in d["qualification"]:
+                if qualification_search.lower() == qualification["qname"].lower():
+                    print(f"""Student Name: {d["name"]}""")
+                    ispresent = True
+        if ispresent == False:
+            print("qualification not found")
+        break
+    else:
+        print("Enter valid qualification: ")
 
-
-
-
-import json
-
-# listdata = []
-# for num in range(1, 4):
-#     studentdata = {}
-    
-#     qualificationlist = []
-#     studentdata["id"] = input("Enter id")
-#     studentdata["name"] = input("Enter name: ")
-#     studentdata["qualification"] = qualificationlist
-    
-#     while True:
-#         qualificationdict = {}
-#         qualificationdict["qname"] = input("Enter qualification: ")
-#         qualificationdict["passingyear"] = int(input("Enter passing year: "))
-#         qualificationlist.append(qualificationdict)
         
-#         ask_qualification = input("Add more qualifications (y/n): ")
-#         if ask_qualification.lower() != 'y':
-#             break
 
-#     listdata.append(studentdata)
-    
-
-# print(json.dumps(listdata, indent=4))
 
    
 

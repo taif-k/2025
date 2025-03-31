ddef963555292aca3ef7e312deac21687b3b9b41
taif-k <taifk817@gmail.com>
2025-03-31 12:09:59 +0530

    trying to use loop for inserting marks

--- python/SRC/highest_percentage.py
+++ python/SRC/highest_percentage.py
@@ -7,41 +7,38 @@ for num in range(1,3):
     studentdata = {}
     markslist = []
     
-
     studentdata["id"] = num + 100
     studentdata["name"] = input("Enter name: ")
     studentdata["marks"] = markslist
     
     marksdict = {}
-    marksdict["hindi"] = int(input("Enter Hindi marks: "))
-    marksdict["english"] = int(input("Enter English marks: "))
-    marksdict["math"] = int(input("Enter Math marks: "))
-    marksdict["science"] = int(input("Enter science marks: "))
+    count = 0
+    while count == 0:
+        marks = int(input("Enter Hindi marks (0 - 100): "))
+        if marks >=0 and marks <=100:
+            marksdict["hindi"] = marks
+            while count == 0:
+                marks = int(input("Enter English marks (0 - 100): "))
+                if marks >=0 and marks <=100:
+                    marksdict["english"] = marks
+                    while count == 0:
+                        marks = int(input("Enter Math marks (0 - 100): "))
+                        if marks >=0 and marks <=100:
+                            marksdict["math"] = marks
+                            while count == 0:
+                                marks = int(input("Enter Science marks (0 - 100): "))
+                                if marks >=0 and marks <=100:
+                                    marksdict["science"] = marks
+                                    count = 1
     markslist.append(marksdict)
-    listdata.append(studentdata)
-    
+    listdata.append(studentdata)    
     percentagelist.append((marksdict["hindi"] + marksdict["english"] + marksdict["math"] + marksdict["science"])/400 * 100)
     
-
-# print(json.dumps(listdata,indent=3))
+print(markslist)
+print(json.dumps(listdata,indent=3))
 percentagelist.sort()
 print(percentagelist)
 print("Highest percentage is: ",percentagelist[-1])    
-
-while True:
-    qualification_search = input("Enter qualification to search: ")
-    if qualification_search.isalnum():
-        ispresent = False
-        for d in listdata:
-            for qualification in d["qualification"]:
-                if qualification_search.lower() == qualification["qname"].lower():
-                    print(f"""Student Name: {d["name"]}""")
-                    ispresent = True
-        if ispresent == False:
-            print("qualification not found")
-        break
-    else:
-        print("Enter valid qualification: ")
 
 
                 

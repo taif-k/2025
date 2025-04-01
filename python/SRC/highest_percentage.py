4b7bd969d9ec4db12be7f86cab4fec4e69152fdb
taif-k <taifk817@gmail.com>
2025-04-01 13:49:02 +0530

    rename file

--- python/SRC/highest_percentage.py
+++ python/SRC/highest_percentage.py
@@ -8,39 +8,45 @@ for num in range(1,3):
     markslist = []
     marksdict = {}
     count = 0
-
-    studentdata["id"] = num + 100
-    studentdata["name"] = input("Enter name: ")
-    studentdata["marks"] = markslist
-    while count == 0:
-        marks = int(input("Enter Hindi marks (0 - 100): "))
-        if marks >=0 and marks <=100:
-            marksdict["hindi"] = marks
-            while count == 0:
-                marks = int(input("Enter English marks (0 - 100): "))
-                if marks >=0 and marks <=100:
-                    marksdict["english"] = marks
-                    while count == 0:
-                        marks = int(input("Enter Math marks (0 - 100): "))
-                        if marks >=0 and marks <=100:
-                            marksdict["math"] = marks
-                            while count == 0:
-                                marks = int(input("Enter Science marks (0 - 100): "))
-                                if marks >=0 and marks <=100:
-                                    marksdict["science"] = marks
-                                    count = 1
-    markslist.append(marksdict)
-    listdata.append(studentdata)    
-    percentagelist.append((marksdict["hindi"] + marksdict["english"] + marksdict["math"] + marksdict["science"])/400 * 100)
     
-print(markslist)
+    while True:
+        studentdata["id"] = input("Enter id: ")
+        if studentdata["id"].isdigit():
+            break
+        else:
+            print("Enter id in digits only")
+    while True:
+            studentdata["name"] = input(f"Enter name {num}: ")
+            if studentdata["name"].isalpha():
+                studentdata["marks"] = markslist
+                for m in range(0,4):
+                    subject = ["hindi","english","math","science"]
+                    m = subject[m]
+                    while True:
+                        marksdict[m] = input(f"Enter {m} marks: ")
+                        if marksdict[m].isdigit():
+                            marksdict[m] = int(marksdict[m])
+                            if marksdict[m] >=0 and marksdict[m] <=100:
+                                break
+                            else:
+                                print("Marks range should be 0 - 100")
+                        else:
+                            print("Marks should be in digits")
+                markslist.append(marksdict)
+                break
+            else:
+                print("Enter valid name")
+        
+
+    percentage = (marksdict["hindi"] + marksdict["english"] + marksdict["math"] + marksdict["science"])/400 * 100
+    percentagelist.append(percentage)
+    listdata.append(studentdata)   
+                
 print(json.dumps(listdata,indent=3))
 percentagelist.sort()
 print(percentagelist)
-print("Highest percentage is: ",percentagelist[-1])    
+print("Highest: ",percentagelist[-1])
 
-
-                
 
 
             

5f22c19377b46a49f351d4a31b76ba96e70c212e
taif-k <taifk817@gmail.com>
2025-04-01 09:31:17 +0530

    fixed infinte loop for id input

--- python/SRC/test.py
+++ python/SRC/test.py
@@ -9,29 +9,31 @@ for num in range(1,3):
     marksdict = {}
     count = 0
 
-    studentdata["id"] = num + 100
     while True:
-        studentdata["name"] = input(f"Enter name {num}: ")
-        if studentdata["name"].isalpha():
-
-            studentdata["marks"] = markslist
-            for m in range(0,4):
-                sub = ["hindi","english","math","science"]
-                m = sub[m]
-                while True:
-                    marksdict[m] = input(f"Enter {m} marks: ")
-                    if marksdict[m].isdigit():
-                        marksdict[m] = int(marksdict[m])
-                        if marksdict[m] >=0 and marksdict[m] <=100:
-                            break
+        studentdata["id"] = input("Enter id: ")
+        if studentdata["id"].isdigit():
+            studentdata["name"] = input(f"Enter name {num}: ")
+            if studentdata["name"].isalpha():
+                studentdata["marks"] = markslist
+                for m in range(0,4):
+                    sub = ["hindi","english","math","science"]
+                    m = sub[m]
+                    while True:
+                        marksdict[m] = input(f"Enter {m} marks: ")
+                        if marksdict[m].isdigit():
+                            marksdict[m] = int(marksdict[m])
+                            if marksdict[m] >=0 and marksdict[m] <=100:
+                                break
+                            else:
+                                print("Marks range should be 0 - 100")
                         else:
-                            print("Marks range should be 0 - 100")
-                    else:
-                        print("Marks should be in digits")
-            markslist.append(marksdict)
-            break
+                            print("Marks should be in digits")
+                markslist.append(marksdict)
+                break
+            else:
+                print("Enter valid name")
         else:
-            print("Enter valid name")
+            print("Enter only digits")
 
     percentage = (marksdict["hindi"] + marksdict["english"] + marksdict["math"] + marksdict["science"])/400 * 100
     percentagelist.append(percentage)
@@ -41,11 +43,6 @@ print(json.dumps(listdata,indent=3))
 percentagelist.sort()
 print(percentagelist)
 print("Highest: ",percentagelist[-1])
-
-  
-
-
-                
 
 
             

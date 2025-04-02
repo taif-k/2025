a77b6433561ec0e8ac9dee7b9f1da961ecb3fb41
taif-k <taifk817@gmail.com>
2025-04-02 11:36:46 +0530

    using func for basic calculation

---
+++ python/SRC/calculator_function.py
@@ -0,0 +1,45 @@
+
+def menu():
+    print("Press 1 for Addition")
+    print("Press 2 for Subtraction")
+    print("Press 3 for Multiplication")
+    print("Press 4 for Division")
+    print("Press 0 for Exit")
+    
+menu()
+
+def addition():
+    first_number = int(input("Enter First Number: "))
+    second_number = int(input("Enter Second Number: "))
+    return first_number + second_number
+
+def subtraction():
+    first_number = int(input("Enter First Number: "))
+    second_number = int(input("Enter Second Number: "))
+    return first_number - second_number
+
+def multiplication():
+    first_number = int(input("Enter First Number: "))
+    second_number = int(input("Enter Second Number: "))
+    return first_number * second_number
+
+def division():
+    first_number = int(input("Enter First Number: "))
+    second_number = int(input("Enter Second Number: ")) 
+    return first_number/second_number
+       
+
+while True:
+    menuinput = int(input("Enter Task no: ")) 
+    if menuinput == 1:
+        print(addition())
+    elif menuinput == 2:
+        print(subtraction())
+    elif menuinput == 3:
+        print(multiplication())
+    elif menuinput == 4:
+        print(division())
+    elif menuinput == 0:
+        break
+    else:
+        print("Choose valid option ")

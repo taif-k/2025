11625394fe4bfffa4487f49f90ccceeb3a7a92a6
taif-k <taifk817@gmail.com>
2025-04-03 11:31:25 +0530

    2 ways to print table

--- python/SRC/create_table.py
+++ python/SRC/create_table.py
@@ -2,10 +2,24 @@ def userinput():
     number = int(input("Enter a number to get its table: "))
     return number
 
-def createtable():
-    number =  userinput()
+def createtable(num):
     for n in range(1,11):
-        print(f"{number * n}")
+        print(f"{num * n}")
+     
+     
+createtable(userinput())
+
+
+# or 
+
+def userinput():
+    number = int(input("Enter a number to get its table: "))
+    return number
+
+def createtable():
+    num  = userinput()
+    for n in range(1,11):
+        print(f"{num * n}")
      
      
 createtable()

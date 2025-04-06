6d74ee0c1c9321b3a6e5fa37eb7d1fe8ae94033b
taif-k <taifk817@gmail.com>
2025-04-06 16:43:13 +0530

    practice

--- python/standard_libraries/date_time.py
+++ python/standard_libraries/date_time.py
@@ -1,8 +1,11 @@
 import datetime
 
 # to get current date & time
-todaydate = datetime.datetime.now()
+todaydate = datetime.datetime.today()
 print(todaydate)
+format_date = todaydate.strftime("%d/%m/%Y")
+print(format_date)
+
 
 # to get previous or future date and time
 tomorrowdate = todaydate + datetime.timedelta(1,0,0,0,0,1)
@@ -12,11 +15,11 @@ previousdate = todaydate + datetime.timedelta(-1) # or todaydate - dt.timedelta(
 print(previousdate)
 
 # to get specific date & time
-year = int(input("Enter year"))
-month = int(input("Enter month"))
-day = int(input("Enter day"))
-specific_date = datetime.datetime(year,month,day) # dt.datetime(2025,4,6)
-print(specific_date)
+# year = int(input("Enter year"))
+# month = int(input("Enter month"))
+# day = int(input("Enter day"))
+# specific_date = datetime.datetime(year,month,day) # datetime.datetime(2025,4,6)
+# print(specific_date)
 
 
 

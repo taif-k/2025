2d837f27cefb9fd8831c5710c6de67bf55d57d6e
taif-k <taifk817@gmail.com>
2025-04-07 09:40:03 +0530

    date time funcs practice

--- python/standard_libraries/datetime_library.py
+++ python/standard_libraries/datetime_library.py
@@ -1,5 +1,6 @@
 import datetime
 import time
+import calendar
 
 # to get current date & time
 todaydate = datetime.datetime.today()
@@ -8,7 +9,7 @@ format_date = todaydate.strftime("%d/%m/%Y")
 print(format_date)
 
 
-# # to get previous or future date and time
+# to get previous or future date and time
 tomorrowdate = todaydate + datetime.timedelta(1,0,0,0,0,1)
 print(tomorrowdate)
 
@@ -26,6 +27,38 @@ print(specific_date)
 data_time = time.time()
 name = input("Enter name") + str(data_time)
 print(name)
+
+# pause execution, sleep
+print("paused for 3 seconds")
+time.sleep(3)
+print("executed after 3 seconds")
+
+
+data = calendar.calendar(2025)
+print(data)
+
+# print(calendar.month(2025,4))
+
+year = 2025
+is_leap = calendar.isleap(year)
+print(f"{year} is a leap year: ",is_leap)
+
+month_range = calendar.monthrange(year,4)
+print(month_range)
+
+id = int(input("Enter id: "))
+count = 0
+while count <= 4:
+    count = count + 1
+    print("Searching.")
+    time.sleep(0.5)
+    print("Searching..")
+    time.sleep(0.5)
+    print("Searching...")
+    
+
+print("Entered id is ",id)
+
 
 
 

63f01b4f4b7521e7ad3f142eb9132e10b3e68fef
taif-k <taifk817@gmail.com>
2025-04-08 08:02:02 +0530

    made modules to create multiple folders at once

---
+++ python/multiple_folders/check_foldername.py
@@ -0,0 +1,19 @@
+import os
+
+def foldername(n):
+    path = r"D:\multiplefolders"
+    while True:
+        user_input = input("Enter number of folders to make: ")     
+        if user_input.isdigit():   
+            user_input = int(user_input)    
+            count = 0
+            while count < user_input:
+                folder_name = f"taif_{n}"
+                folder_path = os.path.join(path, folder_name)
+                if not os.path.exists(folder_path):
+                    os.mkdir(folder_path)
+                    count += 1
+                n += 2
+            break        
+        else:
+            print("Enter valid number")        
---
+++ python/multiple_folders/create_folders.py
@@ -0,0 +1,20 @@
+import check_foldername
+
+path = r"D:\multiplefolders"
+
+def make_folders():
+    while True:
+        even_odd_name = input("1 for odd & 2 for even folder name: ")
+        if even_odd_name.isdigit() and (even_odd_name == "1" or even_odd_name == "2"):
+            even_odd_name = int(even_odd_name)
+            if even_odd_name == 1:
+                n = 1
+                check_foldername.foldername(n)
+                break        
+
+            elif even_odd_name == 2:
+                n = 2
+                check_foldername.foldername(n)
+                break
+
+make_folders()

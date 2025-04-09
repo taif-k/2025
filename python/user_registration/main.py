1b848eecca3719430c3b69f39dabd335fd7fb452
taif-k <taifk817@gmail.com>
2025-04-09 13:47:17 +0530

    removed print stat

--- python/user_registration/main.py
+++ python/user_registration/main.py
@@ -3,7 +3,6 @@ import search_data
 
 def main():
     datalist = []
-    user_input = int(input("Enter 1 to register or 2 to see details:"))
     datalist = student_register.register_user()
     search_data.get_data(datalist)
 

9d3128762f06738eb4050b6a4205c5755a6364c8
taif-k <taifk817@gmail.com>
2025-04-09 08:28:05 +0530

    updated variable value

--- python/file_modes/read.py
+++ python/file_modes/read.py
@@ -4,10 +4,10 @@ paths = [r"D:\multiplefolders\taif.txt",r"D:\Repositories\2025\python\file_modes
 def path_input():
     path_input = int(input("Enter path number (start from 0): "))
     if path_input == 0:
-        n = 0
+        n = path_input
         read_file(n)
     elif path_input == 1:
-        n = 1
+        n = path_input
         read_file(n)
 
 def read_file(n):

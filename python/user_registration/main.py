d77714e1d61d2e1cfe2d328fdb0b07f4711fe32f
taif-k <taifk817@gmail.com>
2025-04-10 09:31:45 +0530

    added task menu function

--- python/user_registration/main.py
+++ python/user_registration/main.py
@@ -1,14 +1,21 @@
 import student_register
 import search_data
 
+def task_option():
+    print("1- Register")
+    print("2- See all Students")
+    print("3 - Search Email")
+
 def main():
-    task_input = int(input("1-register 2-see data 3 -search: "))
+    task_option()
+    task_input = int(input("Enter Task number: "))
     if task_input == 1: 
         student_register.register_user()
     elif task_input == 2:
         student_register.read_allfiles()
     elif task_input == 3:
         search_data.get_data()
+
 
 main()
 

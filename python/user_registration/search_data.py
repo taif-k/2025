543dcd266c0a6c1c8c2faef7e29ca2013d3a935f
taif-k <taifk817@gmail.com>
2025-04-10 12:00:58 +0530

    fixed validation for name, id etc

--- python/user_registration/search_data.py
+++ python/user_registration/search_data.py
@@ -7,69 +7,85 @@ def search_menu():
     print("2- Search name")
     print("3- Search contact")
     print("4- Search address")
+    print("0 - Go Back")
 
 def get_data():
-    search_menu()
-    search_option = int(input("Enter search option"))
-    if search_option == 1:
-        search_email = input("Enter email to search: ")
-        with open(path, 'r') as file: 
-            data = json.load(file)
-            ispresent = False
-            for studentdict in data:
-                for key,value in studentdict.items():
-                    if key == "email":
-                        if value == search_email.lower():
-                            ispresent = True
-                            print(studentdict)
-                            break
+    while True:
+        print()
+        search_menu()
+        search_option = input("Enter search option")
 
-            if ispresent == False:
-                print("Email not found")
-    elif search_option == 2:
-        search_name = input("Enter name to search: ")
-        with open(path, 'r') as file: 
-            data = json.load(file)
-            ispresent = False
-            for studentdict in data:
-                for key,value in studentdict.items():
-                    if key == "name":
-                        if value == search_name.lower():
-                            ispresent = True
-                            print(studentdict)
-                            break
+        if search_option.isdigit():
+            search_option = int(search_option)
+            if search_option == 1:
+                search_email = input("Enter email to search: ")
+                with open(path, 'r') as file: 
+                    data = json.load(file)
+                    ispresent = False
+                    for studentdict in data:
+                        for key,value in studentdict.items():
+                            if key == "email":
+                                if value == search_email.lower():
+                                    ispresent = True
+                                    print(studentdict)
+                                    break
 
-            if ispresent == False:
-                print("name not found")
-    elif search_option == 3:
-        search_contact = int(input("Enter contact to search: "))
-        with open(path, 'r') as file: 
-            data = json.load(file)
-            ispresent = False
-            for studentdict in data:
-                for key,value in studentdict.items():
-                    if key == "contact":
-                        if value == search_contact:
-                            ispresent = True
-                            print(studentdict)
-                            break
+                    if ispresent == False:
+                        print("Email not found")
 
-            if ispresent == False:
-                print("contact not found")
-    elif search_option == 4:
-        search_address = input("Enter address to search: ")
-        with open(path, 'r') as file: 
-            data = json.load(file)
-            ispresent = False
-            for studentdict in data:
-                for key,value in studentdict.items():
-                    if key == "address":
-                        if value == search_address.lower():
-                            ispresent = True
-                            print(studentdict)
-                            break
+            elif search_option == 2:
+                search_name = input("Enter name to search: ")
+                with open(path, 'r') as file: 
+                    data = json.load(file)
+                    ispresent = False
+                    for studentdict in data:
+                        for key,value in studentdict.items():
+                            if key == "name":
+                                if value == search_name.lower():
+                                    ispresent = True
+                                    print(studentdict)
+                                    break
 
-            if ispresent == False:
-                print("Address not found")
+                    if ispresent == False:
+                        print("name not found")
+
+            elif search_option == 3:
+                search_contact = int(input("Enter contact to search: "))
+                with open(path, 'r') as file: 
+                    data = json.load(file)
+                    ispresent = False
+                    for studentdict in data:
+                        for key,value in studentdict.items():
+                            if key == "contact":
+                                if value == search_contact:
+                                    ispresent = True
+                                    print(studentdict)
+                                    break
+
+                    if ispresent == False:
+                        print("contact not found")
+
+            elif search_option == 4:
+                search_address = input("Enter address to search: ")
+                with open(path, 'r') as file: 
+                    data = json.load(file)
+                    ispresent = False
+                    for studentdict in data:
+                        for key,value in studentdict.items():
+                            if key == "address":
+                                if value == search_address.lower():
+                                    ispresent = True
+                                    print(studentdict)
+                                    break
+
+                    if ispresent == False:
+                        print("Address not found")
+
+            elif search_option == 0:
+                break
+            else:
+                print("Invalid option")
+        else:
+            print("Task number should be in digits")
 
 

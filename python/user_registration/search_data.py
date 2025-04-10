f7f9fef58c6f5bc1f31c4714c3d9ee5ad785265a
taif-k <taifk817@gmail.com>
2025-04-10 09:07:57 +0530

    avoided excessive modules

--- python/user_registration/search_data.py
+++ python/user_registration/search_data.py
@@ -4,16 +4,14 @@ def get_data():
     search_email = input("Enter email to search: ")
 
     with open(r"D:\student_details\all_students.json", 'r') as file:
-
         data = json.load(file)
+        ispresent = False
         for studentdict in data:
-                if studentdict.get("email") == search_email:
-                     print(studentdict)
-                     break
-        else:
-             print("email not found")        
-                    
-                     
+            for key,value in studentdict.items():
+                if key == "email":
+                    if value == search_email:
+                        print(studentdict)
 
-                
+        if ispresent == False:
+            print("email Not found")
 

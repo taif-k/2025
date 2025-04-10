4f017e98ab81c5f86719e3e5c6593668e4802b5b
taif-k <taifk817@gmail.com>
2025-04-10 08:50:44 +0530

    changed variable name

--- python/user_registration/search_data.py
+++ python/user_registration/search_data.py
@@ -4,11 +4,11 @@ def get_data():
     search_email = input("Enter email to search: ")
 
     with open(r"D:\student_details\all_students.json", 'r') as file:
-        
+
         data = json.load(file)
-        for dict in data:
-                if dict.get("email") == search_email:
-                     print(dict)
+        for studentdict in data:
+                if studentdict.get("email") == search_email:
+                     print(studentdict)
                      break
         else:
              print("email not found")        

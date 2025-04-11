d6ea5e370f4a632663a8d9d5f4a655cdba9f0f4a
taif-k <taifk817@gmail.com>
2025-04-11 10:36:09 +0530

    added line number variable

--- python/SRC/error_source.py
+++ python/SRC/error_source.py
@@ -8,6 +8,7 @@ def err_source(e):
     module_name = os.path.basename(__file__)
     function_name = inspect.getframeinfo(frame).function
     date = datetime.datetime.now()
+    lineno = inspect.getframeinfo(frame).lineno
 
-    detail = str({"module": module_name,"function": function_name,"error": str(e),"date": date})
+    detail = str({"module": module_name,"function": function_name,"error": str(e),"date": date,"line":lineno})
     return detail

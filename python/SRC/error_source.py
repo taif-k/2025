1f9a632127955fabef0be318d61e8c59004b903d
taif-k <taifk817@gmail.com>
2025-04-11 09:18:11 +0530

    created a module to get error source dynamically

---
+++ python/SRC/error_source.py
@@ -0,0 +1,13 @@
+import inspect
+import os
+import datetime
+
+def err_source(e):
+
+    frame = inspect.currentframe()
+    module_name = os.path.basename(__file__)
+    function_name = inspect.getframeinfo(frame).function
+    date = datetime.datetime.now()
+
+    detail = str({"module": module_name,"function": function_name,"error": str(e),"date": date})
+    return detail

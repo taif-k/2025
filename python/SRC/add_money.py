9e976f41d518d5883280c4ec56f0f801ddf15ea1
taif-k <taifk817@gmail.com>
2025-04-12 13:50:35 +0530

    exception handling

---
+++ python/SRC/add_money.py
@@ -0,0 +1,23 @@
+import error_log
+import time
+import datetime
+
+
+def input_details():
+
+    try:
+        userdict = {}
+        userdict["name"] = "taif"
+        userdict["accountno"] = 12345678901
+
+        userdict["amount"] = int(input("Add amount: "))
+        print("processing...")
+        time.sleep(1)
+        print(userdict) 
+    except Exception as e:
+        print("Unable to add money...")
+        date = datetime.datetime.now()
+        errordetails = str({"module":"add_money.py","function":"input_details","error":e,"date":date})
+        error_log.create_log(errordetails)    
+
+input_details()

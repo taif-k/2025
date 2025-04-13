73ffb5779f2f9fa57c271f59343bc045ffd5a065
taif-k <taifk817@gmail.com>
2025-04-13 10:23:18 +0530

    fixed merge conflict

--- python/SRC/squared_numbers.py
+++ python/SRC/squared_numbers.py
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 import datetime
-=======
 import error_source
->>>>>>> 7959194c4f8b6bfae647041d94a7f9f60580ce6b
 import error_log
 
 path = r"D:\Repositories\2025\python\SRC\errordetails.txt"
@@ -14,13 +11,9 @@ def input_number():
         print("Squared number is: ",squared_numbers)
     except Exception as e:
         print("Number should be in digits") 
-
-<<<<<<< HEAD
         date = datetime.datetime.now()
         errordict = str({"module":"squared_numbers.py","function":"input_number()","error":e,"errortime":date})
-=======
         errordict = error_source.err_source(e)
->>>>>>> 7959194c4f8b6bfae647041d94a7f9f60580ce6b
         error_log.create_log(errordict)
         input_number()
               

0a452abd6c34a447219f01e0f9e8a7d0aafa5659
taif-k <taifk817@gmail.com>
2025-04-18 16:16:52 +0530

    removed duplicate code

--- python/Students_Record_System/Src/Report/ActiveStudent/active_studentdetails.py
+++ python/Students_Record_System/Src/Report/ActiveStudent/active_studentdetails.py
@@ -1,14 +1,7 @@
 import Domain
-import datetime
 log_path = r"D:\Repositories\2025\python\Students_Record_System\Src\Database\errorslog.txt"
 
-def create_datetime():
-    date = datetime.datetime.now()
-    shortdate = date.strftime("%d/%m/%Y, %H:%M:%S")
-    return shortdate
-
 def active_student():
-    #print(Domain.studentlist)
     try:
         activelist = []
         data = Domain.studentlist
@@ -19,9 +12,9 @@ def active_student():
                         activelist.append(studentdict)
 
         return activelist
-        #print(activelist)
+    
     except Exception as e:
-        date = create_datetime()
+        date = Domain.create_datetime()
         errordeatils = str({"mod":"active_studentdetails.py","error":e,"date":date})
         Domain.update_errorlog(errordeatils,log_path)
         

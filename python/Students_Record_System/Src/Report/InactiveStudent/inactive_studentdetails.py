fa0b8965b1c3e7a1daa8d51889f49aa7d6181750
taif-k <taifk817@gmail.com>
2025-04-18 16:17:34 +0530

    duplicate code removed

--- python/Students_Record_System/Src/Report/InactiveStudent/inactive_studentdetails.py
+++ python/Students_Record_System/Src/Report/InactiveStudent/inactive_studentdetails.py
@@ -1,11 +1,5 @@
 import Domain
-import datetime
 log_path = r"D:\Repositories\2025\python\Students_Record_System\Src\Database\errorslog.txt"
-
-def create_datetime():
-    date = datetime.datetime.now()
-    shortdate = date.strftime("%d/%m/%Y, %H:%M:%S")
-    return shortdate
 
 def inactive_student():
     try:
@@ -19,6 +13,6 @@ def inactive_student():
         return inactivelist
     
     except Exception as e:
-        date = create_datetime()
+        date = Domain.create_datetime()
         errordetails = str({"mod":"inactive_studentdetails.py","error":e,"date":date})
         Domain.update_errorlog(errordetails,log_path)

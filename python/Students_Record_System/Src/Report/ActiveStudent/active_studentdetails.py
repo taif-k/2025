42cbaca8e2a8ca73b01123ae7de07bc57ae6d230
taif-k <taifk817@gmail.com>
2025-04-19 21:31:02 +0530

    removed unused code

--- python/Students_Record_System/Src/Report/ActiveStudent/active_studentdetails.py
+++ python/Students_Record_System/Src/Report/ActiveStudent/active_studentdetails.py
@@ -1,20 +1,14 @@
 import Domain
-log_path = r"D:\Repositories\2025\python\Students_Record_System\Src\Database\errorslog.txt"
 
 def active_student():
-    try:
-        activelist = []
-        data = Domain.studentlist
-        for studentdict in data:
-            for key,value in studentdict.items():
-                if key == "isActive":
-                    if value == "True":
-                        activelist.append(studentdict)
+    activelist = []
+    data = Domain.studentlist
+    for studentdict in data:
+        for key,value in studentdict.items():
+            if key == "isActive":
+                if value == "True":
+                    activelist.append(studentdict)
 
-        return activelist
-    
-    except Exception as e:
-        date = Domain.create_datetime()
-        errordeatils = str({"mod":"active_studentdetails.py","error":e,"date":date})
-        Domain.update_errorlog(errordeatils,log_path)
+    return activelist
+
         
--- python/Students_Record_System/Src/Report/InactiveStudent/inactive_studentdetails.py
+++ python/Students_Record_System/Src/Report/InactiveStudent/inactive_studentdetails.py
@@ -1,19 +1,13 @@
 import Domain
-import sys,os
-sys.path.append(os.getcwd())
-import python
-log_path = r"D:\Repositories\2025\python\Students_Record_System\Src\Database\errorslog.txt"
 
 def inactive_student():
-    try:
-        inactivelist = []
-        data = Domain.studentlist
-        for studentdict in data:
-            for key,value in studentdict.items():
-                if key == "isActive":
-                    if value == "False":
-                        inactivelist.append(studentdict)
-        return inactivelist
-    
-    except Exception as e:
-        Domain.update_errorlog(python.error_details(e),log_path)
+    inactivelist = []
+    data = Domain.studentlist
+    for studentdict in data:
+        for key,value in studentdict.items():
+            if key == "isActive":
+                if value == "False":
+                    inactivelist.append(studentdict)
+    return inactivelist
+
+        

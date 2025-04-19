75c5422d43d79946eee70ff216e230836c7d66cd
taif-k <taifk817@gmail.com>
2025-04-19 08:15:45 +0530

    using traceback module

--- python/Programs/error_source.py
+++ python/Programs/error_source.py
@@ -1,14 +1,24 @@
-import inspect
+import traceback
 import os
 import datetime
+err_logpath = r"D:\Repositories\2025\python\Programs\errordetails.txt"
 
-def err_source(e):
-
-    frame = inspect.currentframe()
-    module_name = os.path.basename(__file__)
-    function_name = inspect.getframeinfo(frame).function
+def error_details(e):
+    tb = traceback.extract_tb(e.__traceback__)[-1]
+    module_name = os.path.basename(tb.filename)
+    function_name = tb.name
+    lineno = tb.lineno
     date = datetime.datetime.now()
-    lineno = inspect.getframeinfo(frame).lineno
+    stringdate = date.strftime("%d/%m/%Y, %H:%M:%S")
 
-    detail = str({"module": module_name,"function": function_name,"error": str(e),"date": date,"line":lineno})
-    return detail
+    err_detail = str({"module": module_name,"function": function_name,"error": str(e),"date":stringdate,"line": lineno})
+    return err_detail
+
+def read_errors():
+    with open(err_logpath,"r") as file:
+        data = file.read()
+        print(data)
+
+def update_errorslog(data):
+    with open(err_logpath,"a") as file:
+        file.write(f"\n{data}") 

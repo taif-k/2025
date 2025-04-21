5526d75285571e3db424dbb33bf5529ffa6c7915
taif-k <taifk817@gmail.com>
2025-04-21 07:13:06 +0530

    returning message if data is missing

--- python/Students_Record_System/Src/Report/InactiveStudent/inactive_studentdetails.py
+++ python/Students_Record_System/Src/Report/InactiveStudent/inactive_studentdetails.py
@@ -3,6 +3,10 @@ import Domain
 def inactive_student():
     inactivelist = []
     data = Domain.studentlist
+    
+    if not data:
+        return "Data not available for Search....Register Students please"
+    
     for studentdict in data:
         for key,value in studentdict.items():
             if key == "isActive":

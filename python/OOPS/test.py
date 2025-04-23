11cac6395970e5fbfba90afa267db797e2933796
taif-k <taifk817@gmail.com>
2025-04-23 17:46:19 +0530

    uncomment

--- python/OOPS/test.py
+++ python/OOPS/test.py
@@ -1,27 +1,21 @@
 class number:
     def __init__(self):
         print("hello")
-
     num = 3    
 
 data = number()
 print(data.num)
 
-
-
-
 class details:
 
     def __init__(self,ID,NAME):
         print(f"ID: {ID} and name: {NAME}")
-    # print("hi")
+    print("hi")
 
-# details1 = details(101,"test")
+details1 = details(101,"test")
 
 # first, class variables are printed thn object's  
-
 # printing order will be:
-# 3
 # hello
 # 3
 # hi

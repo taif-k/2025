87e74642e70a957e90903d344439315446832f97
taif-k <taifk817@gmail.com>
2025-04-23 08:28:52 +0530

    fixed student search

--- python/OOPS/register_student.py
+++ python/OOPS/register_student.py
@@ -2,27 +2,7 @@ import json
 
 class student:
     def __init__(self):
-        self.menu()
-
-    def menu(self):
-        print("1- Register")
-        print("2- Display")
-        print("3- Search Student")
-        print("0- Exit")
-        self.menu_option()
-
-    def menu_option(self):
-        while True:
-            ask_option = int(input("Enter option: ")) 
-            if ask_option == 1:
-                self.ask_details()
-            elif ask_option == 2:
-                self.display_info()       
-            elif ask_option == 3:
-                pass
-                # self.search_student()
-            elif ask_option == 0:
-                break
+        self.ask_details()
 
     def ask_details(self):
         self.studentlist = []
@@ -36,26 +16,29 @@ class student:
             studentdict["grade"] = self.grade
             self.studentlist.append(studentdict)
 
-            regsiter_student = input("Add Student y/n: ")
+            regsiter_student = input("Add More Students y/n: ")
             if regsiter_student == "y":
                 continue
             else:
-                self.menu()
                 break
-        
+        self.display_info()
+
     def display_info(self):
         print(json.dumps(self.studentlist,indent=4))
+        self.search_student()
 
-    # def search_student(self):
-    #     search_name = input("Enter name to serach: ")
-    #     iPresent = 0
-    #     for data in self.studentlist:
-    #         for key,value in data:
-    #             if key == "name":
-    #                 if value == search_name:
-    #                     print(data)
+    def search_student(self):
+        search_name = input("Enter name to serach: ")
+        isPresent = 0
+        for data in self.studentlist:
+            for key,value in data.items():
+                if key == "name":
+                    if value == search_name:
+                        isPresent = 1
+                        print(json.dumps(data,indent=3))
+                        break
 
-    #     if iPresent == 0:
-    #         print("Not found")
+        if isPresent == 0:
+            print("Not found")
 
 studentdata = student()

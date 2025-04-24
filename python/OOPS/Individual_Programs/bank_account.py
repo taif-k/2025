65cfe744b46190f1e924f0ca389a003b0824b825
taif-k <taifk817@gmail.com>
2025-04-24 19:47:24 +0530

    made balance /acc_no private

---
+++ python/OOPS/Individual_Programs/bank_account.py
@@ -0,0 +1,45 @@
+class BankAccout:
+    def __init__(self):
+        self.__account_no = 1234567890
+        self.__totalbalance = 500
+        
+    def task_option(self):
+        print("1-withdraw")
+        print("2-Deposit")
+        print("3-check balance")
+
+    def verifyaccount(self):
+        self.__pin = 123
+        enter_pin = int(input("Enter pin to verify: "))
+        if enter_pin == self.__pin:
+            while True:
+                self.task_option()
+                ask_option = int(input("Enter option: "))
+                if ask_option == 1:
+                    self.withdrawl()
+                elif ask_option == 2:
+                    self.deposit()
+                elif ask_option == 3:
+                    balance = self.__totalbalance
+                    print(f"Total balance is {balance}")
+                else:
+                    break
+        else:
+            print("Incorrect pin")                
+
+    def withdrawl(self):
+        self.withdraw_amount = int(input("Enter amount to withdrawl: "))
+        if self.withdraw_amount <= self.__totalbalance: 
+            self.__totalbalance -= self.withdraw_amount
+        else:
+            print("Withdraw amount greater than total balance")
+
+    def deposit(self):
+        self.deposit_amount = int(input("Enter amount to deposit: "))
+        if self.deposit_amount >= 0:   
+            self.__totalbalance += self.deposit_amount
+        else:
+            print("Enter valid amount")
+
+account1 = BankAccout()
+account1.verifyaccount()

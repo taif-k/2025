e1a9a6f0ee6d2d019e4c5e6a039ff0fe3ba716be
taif-k <taifk817@gmail.com>
2025-04-27 15:47:51 +0530

    seprate class for search func

---
+++ python/OOPS/Student_Records_System/Report/search_student.py
@@ -0,0 +1,17 @@
+import json
+import python
+
+class StudentSearch:
+    def __init__(self,datapath,errorlog_path):
+        self.recordspath = datapath
+        self.err_path = errorlog_path
+
+    def search_student(self):
+        search_name = input("Enter name to search: ")
+        listobj = python.Student(self.recordspath, self.err_path)
+        for data in listobj.studentlist:
+            if data["name"] == search_name:
+                print(json.dumps(data,indent=3))
+                print()
+                return None
+        print("\nNot found")

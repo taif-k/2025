be973d2a3a47b037daed7639b1d7c308bc434508
taif-k <taifk817@gmail.com>
2025-04-27 08:03:23 +0530

    using child class

--- python/OOPS/Student_Records_System/main.py
+++ python/OOPS/Student_Records_System/main.py
@@ -7,5 +7,5 @@ import python
 datapath = r"D:\Repositories\2025\python\OOPS\Student_Records_System\Database\studentdata.json"
 errorlog_path = r"D:\Repositories\2025\python\OOPS\Student_Records_System\Log\errorsdetail.txt"
 
-student_one = python.student(datapath,errorlog_path)
+student_one = python.StudentMenu(datapath,errorlog_path)
 student_one.menu_option()

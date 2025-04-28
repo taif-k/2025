136a5b8cd3adcd40f640f4af99353f630729e88f
taif-k <taifk817@gmail.com>
2025-04-28 08:10:24 +0530

    mobed write/read func in sep class and module

--- python/OOPS/Student_Records_System/Domain/student_registration.py
+++ python/OOPS/Student_Records_System/Domain/student_registration.py
@@ -4,25 +4,9 @@ import python as py_lang
 class Student:
     def __init__(self,datapath,errorlog_path):
         self.recordspath = datapath
-        self.studentlist = self.read_alldata()
         self.err_path = errorlog_path
 
-    def write_data(self):
-        with open(self.recordspath,"w") as file:
-            file.write(json.dumps(self.studentlist,indent=3))
-
-    def read_alldata(self):
-        try:
-            with open(self.recordspath, "r") as file:
-                studentrecords = json.load(file)
-                return studentrecords
-        except Exception as e:
-            print("Student Records are blank")
-            py_lang.update_errorslog(py_lang.error_details(e),self.err_path)
-            return []   
-
-    def ask_details(self):
-        
+    def ask_details(self):        
             while True:
                 try:
                     studentdict = {}
@@ -30,7 +14,8 @@ class Student:
                     studentdict["age"] = int(input("Enter Age: "))
                     studentdict["grade"] = input("Enter Grade: ")
                     studentdict["joineddate"] = py_lang.error_details(get_onlydate=True)
-                    self.studentlist.append(studentdict)
+                    self.listobj = py_lang.DataOperations(self.recordspath,self.err_path)
+                    self.listobj.studentlist.append(studentdict)
 
                     if input("Add More Students y/n: ").lower() != "y":
                         break
@@ -38,7 +23,7 @@ class Student:
                 except Exception as e:
                     print("Enter valid details...")
                     py_lang.update_errorslog(py_lang.error_details(e),self.err_path) # error_details() using traceback module
-            self.write_data()            
+            self.listobj.write_data()            
 
         
 class StudentMenu(Student):
@@ -55,7 +40,8 @@ class StudentMenu(Student):
             if ask_menu == 1:
                 self.ask_details()  
             elif ask_menu == 2:
-                print(json.dumps(self.studentlist,indent=4))
+                listobj = py_lang.DataOperations(self.recordspath,self.err_path)
+                print(json.dumps(listobj.studentlist,indent=4))
                 print()
             elif ask_menu ==3:
                 searchobj = py_lang.StudentSearch(self.recordspath, self.err_path)

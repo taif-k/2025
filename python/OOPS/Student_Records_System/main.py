b147909d5e9bf040ebe533492582cc0df6ce5bae
taif-k <taifk817@gmail.com>
2025-04-28 07:12:23 +0530

    used alias for user define package

--- python/OOPS/Student_Records_System/main.py
+++ python/OOPS/Student_Records_System/main.py
@@ -2,10 +2,10 @@ import sys
 import os
 sys.path.append(os.getcwd())
 
-import python
+import python as py_lang
 
 datapath = r"D:\Repositories\2025\python\OOPS\Student_Records_System\Database\studentdata.json"
 errorlog_path = r"D:\Repositories\2025\python\OOPS\Student_Records_System\Log\errorsdetail.txt"
 
-student_one = python.StudentMenu(datapath,errorlog_path)
+student_one = py_lang.StudentMenu(datapath,errorlog_path)
 student_one.menu_option()

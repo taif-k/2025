8dcbd61d7c58ebc039fe7c0ec0e03d1986e2460c
taif-k <taifk817@gmail.com>
2025-04-28 08:27:48 +0530

    using diff class for getting list

--- python/OOPS/Student_Records_System/Report/search_student.py
+++ python/OOPS/Student_Records_System/Report/search_student.py
@@ -8,7 +8,7 @@ class StudentSearch:
 
     def search_student(self):
         search_name = input("Enter name to search: ")
-        listobj = py_lang.Student(self.recordspath, self.err_path)
+        listobj = py_lang.DataOperations(self.recordspath, self.err_path)
         for data in listobj.studentlist:
             if data["name"] == search_name:
                 print(json.dumps(data,indent=3))

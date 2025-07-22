c6b98d924ca97cc654c59b8425c762ac28d98570
taif-k <taifk817@gmail.com>
2025-07-22 11:46:12 +0530

    handling exceptions & logging errors

--- python/student_qualification/src/Domain/register_students.py
+++ python/student_qualification/src/Domain/register_students.py
@@ -1,32 +1,35 @@
 import json
-
+from python.Programs.error_source import error_details,update_errorslog
+from python.student_qualification.src.Domain.paths import diff_path_obj
 
 class Registration:
     def __init__(self):
         pass
 
     def student_registration(self):
-        self.studentlist = []
-        for num in range(1, 4):
-            studentdata = {}
-            qualificationlist = []
-            studentdata["id"] = num
-            studentdata["name"] = input("Enter name: ")
-            studentdata["qualification"] = qualificationlist
+        try:
+            self.studentlist = []
+            for num in range(1, 4):
+                studentdata = {}
+                qualificationlist = []
+                studentdata["id"] = num
+                studentdata["name"] = input("Enter name: ")
+                studentdata["qualification"] = qualificationlist
 
-            while True:
-                qualificationdict = {}
-                qualificationdict["qname"] = input("Enter qualification: ")
-                qualificationdict["passingyear"] = input("Enter passing year")
-                qualificationlist.append(qualificationdict)
+                while True:
+                    qualificationdict = {}
+                    qualificationdict["qname"] = input("Enter qualification: ")
+                    qualificationdict["passingyear"] = input("Enter passing year")
+                    qualificationlist.append(qualificationdict)
 
-                ask_qualification = input("Add More qualification - y or n: ")
+                    ask_qualification = input("Add More qualification - y or n: ")
 
-                if ask_qualification.lower() != "y":
-                    break
+                    if ask_qualification.lower() != "y":
+                        break
 
-            self.studentlist.append(studentdata)
-            print(json.dumps(self.studentlist, indent=4))
-
+                self.studentlist.append(studentdata)
+                print(json.dumps(self.studentlist, indent=4))
+        except Exception as e:
+            update_errorslog(error_details(e),diff_path_obj.errorlog_path)
 
 student_obj = Registration()

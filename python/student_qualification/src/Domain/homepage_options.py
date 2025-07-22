6e066c6284a3b76043ef8c042c7aec116a937192
taif-k <taifk817@gmail.com>
2025-07-22 11:46:02 +0530

    handling exceptions & logging errors

--- python/student_qualification/src/Domain/homepage_options.py
+++ python/student_qualification/src/Domain/homepage_options.py
@@ -1,5 +1,7 @@
 from python.student_qualification.src.Domain.register_students import student_obj
 from python.student_qualification.src.Domain.search import search_obj
+from python.student_qualification.src.Domain.paths import diff_path_obj
+from python.Programs.error_source import error_details,update_errorslog
 
 class Homepage:
     def __init__(self):
@@ -10,13 +12,16 @@ class Homepage:
         print("2 - Search Student by Qualification")
 
     def main_menu(self):
-        self.menu_option()
-        ask_menu = int(input("Enter Menu option: "))
-        if ask_menu == 1:
-            student_obj.student_registration()
-        elif ask_menu == 2:
-            search_obj.student_search()
-        else:
-            print("Choose valid option")
+        try:
+            self.menu_option()
+            ask_menu = int(input("Enter Menu option: "))
+            if ask_menu == 1:
+                student_obj.student_registration()
+            elif ask_menu == 2:
+                search_obj.student_search()
+            else:
+                print("Choose valid option")
+        except Exception as e:
+            update_errorslog(error_details(e),diff_path_obj)
 
 homepage_obj = Homepage()

e6d587a4c5508f8cb5a88b0ef91f61df244bfe0e
taif-k <taifk817@gmail.com>
2025-07-22 11:09:23 +0530

    converting in CLASS

--- python/student_qualification/src/Domain/homepage_options.py
+++ python/student_qualification/src/Domain/homepage_options.py
@@ -1,19 +1,24 @@
-import os,sys
-sys.path.append(os.getcwd())
-
 from python.student_qualification.src.Domain.register_students import student_registration
 from python.student_qualification.src.Domain.search import student_search
 
-def menu_option():
-    print("1 - Register Student")
-    print("2 - Search Student by Qualification")
 
-def main_menu():
-    menu_option()
-    ask_menu = int(input("Enter Mmenu options: "))
-    if ask_menu == 1:
-        student_registration()
-    elif ask_menu == 2:
-        student_search()
-    else:
-        print("Choose valid option")
+class Homepage: 
+    def __init__(self):
+        pass
+
+    def menu_option(self):
+        print("1 - Register Student")
+        print("2 - Search Student by Qualification")
+
+    def main_menu(self):
+        self.menu_option()
+        ask_menu = int(input("Enter Mmenu options: "))
+        if ask_menu == 1:
+            student_registration()
+        elif ask_menu == 2:
+            student_search()
+        else:
+            print("Choose valid option")
+
+homepage_obj = Homepage()
+

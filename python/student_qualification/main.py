736b4c473fc3a8e633d1ac90bde3bc33a01fa967
taif-k <taifk817@gmail.com>
2025-07-22 11:18:34 +0530

    using class

--- python/student_qualification/main.py
+++ python/student_qualification/main.py
@@ -1,6 +1,5 @@
 import os,sys
 sys.path.append(os.getcwd())
+from python.student_qualification.src.Domain.homepage_options import homepage_obj
 
-from python.student_qualification.src.Domain.homepage_options import main_menu
-
-main_menu()
+homepage_obj.main_menu()

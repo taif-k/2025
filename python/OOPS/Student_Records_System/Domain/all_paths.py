003b73e7de37cd37f5ae4820e309ddd201b5965b
taif-k <taifk817@gmail.com>
2025-07-22 19:47:29 +0530

    created sep class  for paths

---
+++ python/OOPS/Student_Records_System/Domain/all_paths.py
@@ -0,0 +1,5 @@
+class Path:
+    datapath = r"D:\Repositories\2025\python\OOPS\Student_Records_System\Database\studentdata.json"
+    errorlog_path = r"D:\Repositories\2025\python\OOPS\Student_Records_System\Log\errorsdetail.txt"
+
+paths_obj = Path()

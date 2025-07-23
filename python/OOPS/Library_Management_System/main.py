652f09eaef7e0a11d67fdb48a69558d3fb0028d3
taif-k <taifk817@gmail.com>
2025-07-23 11:28:35 +0530

    directly importing obj

--- python/OOPS/Library_Management_System/main.py
+++ python/OOPS/Library_Management_System/main.py
@@ -1,7 +1,5 @@
 import sys,os
 sys.path.append(os.getcwd())
-from python.OOPS.Library_Management_System.src.Domain.option_menu import BookMenu
-bookdata = r"D:\Repositories\2025\python\OOPS\Library_Management_System\src\Database\bookdata.json"
+from python.OOPS.Library_Management_System.src.Domain.option_menu import bookmenu_obj
 
-book_obj = BookMenu(bookdata)
-book_obj.menu_option()
+bookmenu_obj.menu_option()

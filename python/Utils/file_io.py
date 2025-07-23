c7ed1051a5247f4683d191f7d77c3be4f90efc1f
taif-k <taifk817@gmail.com>
2025-07-23 19:30:53 +0530

    updated comments

--- python/Utils/file_io.py
+++ python/Utils/file_io.py
@@ -5,23 +5,23 @@ import os
 
 error_path = r"D:\Repositories\2025\python\Log\error_log.txt"
 
-class FileIO:        
-    # def write_data(data, path):
-    #     with open(path, "w") as file:
-    #         file.write(f"\n{data}")
 
-    def __init__(self,error_path):
-        self.error_path = error_path 
+class FileIO:
+    def __init__(self, error_path):
+        self.error_path = error_path
 
-    def write_data(self,data,path):
+    # storing data in local files 
+    def write_data(self, data, path):
         with open(path, "w") as file:
             file.write(json.dumps(data, indent=4))
 
-    def update_errorslog(self,data):
-        with open(self.error_path, "a") as file:
-            file.write(f"\n{data}")    
+    # logging errors details
+    def update_errorslog(self, data, path=error_path):
+        with open(path, "a") as file:
+            file.write(f"\n{data}")
 
-    def read_alldata(self,path):
+    # read file data
+    def read_alldata(self, path):
         try:
             with open(path, "r") as file:
                 data = json.load(file)
@@ -30,21 +30,23 @@ class FileIO:
             print("check error log..")
             file_io_obj.update_errorslog(file_io_obj.get_errdetails(e))
             return []
-
-    def get_errdetails(self,error = None, get_date = False):
+        
+    # traceback module to get dyn. err details
+    def get_errdetails(self, error=None, get_date=False):
         date = datetime.datetime.now()
         str_date = date.strftime("%Y-%m-%d %H:%M:%S")
 
         if get_date == True:
             return str_date
-        
+
         if error is not None:
             tb = traceback.extract_tb(error.__traceback__)[-1]
             module_name = os.path.basename(tb.filename)
             function_name = tb.name
             line_no = tb.lineno
 
-            err_details = json.dumps({"module":module_name,"function":function_name,"error":str(error),"date":str_date,"line":line_no})
+            err_details = json.dumps({"module": module_name, "function": function_name, "error": str(
+                error), "date": str_date, "line": line_no})
             return err_details
 
 

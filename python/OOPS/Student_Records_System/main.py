33d371a9d921bf91e5869a28346559ffa16cf61e
taif-k <taifk817@gmail.com>
2025-07-23 16:50:46 +0530

    removed 2nd arg

--- python/OOPS/Student_Records_System/main.py
+++ python/OOPS/Student_Records_System/main.py
@@ -5,5 +5,5 @@ sys.path.append(os.getcwd())
 from python.OOPS.Student_Records_System.Domain.student_registration import StudentMenu
 from python.OOPS.Student_Records_System.Domain.all_paths import paths_obj
 
-student_obj = StudentMenu(paths_obj.datapath,paths_obj.errorlog_path)
+student_obj = StudentMenu(paths_obj.datapath)
 student_obj.menu_option()

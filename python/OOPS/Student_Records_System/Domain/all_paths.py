978ae00480e729e9de72911e1c764a4addc3f804
taif-k <taifk817@gmail.com>
2025-07-23 16:49:14 +0530

    logging entire "python" errors in single log

--- python/OOPS/Student_Records_System/Domain/all_paths.py
+++ python/OOPS/Student_Records_System/Domain/all_paths.py
@@ -1,5 +1,4 @@
 class Path:
     datapath = r"D:\Repositories\2025\python\OOPS\Student_Records_System\Database\studentdata.json"
-    errorlog_path = r"D:\Repositories\2025\python\OOPS\Student_Records_System\Log\errorsdetail.txt"
 
 paths_obj = Path()

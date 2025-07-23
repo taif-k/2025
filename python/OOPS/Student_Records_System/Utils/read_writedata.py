7a7aaec86562d8923cf12de26a7cd8d965e25933
taif-k <taifk817@gmail.com>
2025-07-23 17:18:02 +0530

    removed dup write file func

--- python/OOPS/Student_Records_System/Utils/read_writedata.py
+++ python/OOPS/Student_Records_System/Utils/read_writedata.py
@@ -1,16 +1,14 @@
-import json
-from python.Programs.error_source import update_errorslog, error_details
 from python.OOPS.Student_Records_System.Domain.all_paths import paths_obj
 from python.Utils.file_io import file_io_obj
 
 
 class DataOperations:
     def __init__(self, datapath):
-        self.recordspath = datapath
-        self.studentlist = file_io_obj.read_alldata(self.recordspath)
-
-    def write_data(self):
-        with open(self.recordspath, "w") as file:
-            file.write(json.dumps(self.studentlist, indent=3))
+        try:
+            self.recordspath = datapath
+            self.studentlist = file_io_obj.read_alldata(self.recordspath)
+        except Exception as e:
+            print("Error logged...Try again after some time")
+            file_io_obj.update_errorslog(file_io_obj.get_errdetails(e))
 
 file_obj = DataOperations(paths_obj.datapath)

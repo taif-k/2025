9bd7b69f0f0f24a5d20166cb016fba9a598379b5
taif-k <taifk817@gmail.com>
2025-07-23 16:53:21 +0530

    resued read file func from diff package

--- python/OOPS/Student_Records_System/Utils/read_writedata.py
+++ python/OOPS/Student_Records_System/Utils/read_writedata.py
@@ -1,23 +1,16 @@
 import json
-from python.Programs.error_source import update_errorslog,error_details
+from python.Programs.error_source import update_errorslog, error_details
+from python.OOPS.Student_Records_System.Domain.all_paths import paths_obj
+from python.Utils.file_io import file_io_obj
+
 
 class DataOperations:
-        def __init__(self,datapath,errorlog_path):
-            self.recordspath = datapath
-            self.err_path = errorlog_path
-            self.studentlist = self.read_alldata()
+    def __init__(self, datapath):
+        self.recordspath = datapath
+        self.studentlist = file_io_obj.read_alldata(self.recordspath)
 
-        def write_data(self):
-            with open(self.recordspath,"w") as file:
-                file.write(json.dumps(self.studentlist,indent=3))
+    def write_data(self):
+        with open(self.recordspath, "w") as file:
+            file.write(json.dumps(self.studentlist, indent=3))
 
-        def read_alldata(self):
-            try:
-                with open(self.recordspath, "r") as file:
-                    studentrecords = json.load(file)
-                    return studentrecords
-            except Exception as e:
-                print("Student Records are blank")
-                update_errorslog(error_details(e),self.err_path)
-                return []   
-            
+file_obj = DataOperations(paths_obj.datapath)

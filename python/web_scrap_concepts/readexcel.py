24d98de2cd61ecc2dd8788deb966000060b7db6a
taif-k <taifk817@gmail.com>
2025-07-24 08:12:08 +0530

    updated path

--- python/web_scrap_concepts/readexcel.py
+++ python/web_scrap_concepts/readexcel.py
@@ -1,5 +1,5 @@
-path = r"C:\Users\DELL\Desktop\pandastest.xlsx"
 import pandas as pd
+path = r"D:\Repositories\2025\python\web_scrap_concepts\npi_data.xlsx"
 
 exceldata = pd.read_excel(path,sheet_name="Sheet1")
 print(pd.DataFrame(exceldata))

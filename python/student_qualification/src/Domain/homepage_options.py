5d54f3731d3c23510e290fb7257f9c897577a9ba
taif-k <taifk817@gmail.com>
2025-07-24 07:12:40 +0530

    fixed  getting "None" in log

--- python/student_qualification/src/Domain/homepage_options.py
+++ python/student_qualification/src/Domain/homepage_options.py
@@ -1,7 +1,8 @@
 from python.student_qualification.src.Domain.register_students import student_obj
 from python.student_qualification.src.Domain.search import search_obj
 from python.student_qualification.src.Domain.paths import diff_path_obj
-from python.Programs.error_source import error_details,update_errorslog
+from python.Utils.file_io import file_io_obj
+
 
 class Homepage:
     def __init__(self):
@@ -10,18 +11,22 @@ class Homepage:
     def menu_option(self):
         print("1 - Register Student")
         print("2 - Search Student by Qualification")
+        print("0 - Exit")
 
     def main_menu(self):
         try:
-            self.menu_option()
-            ask_menu = int(input("Enter Menu option: "))
-            if ask_menu == 1:
-                student_obj.student_registration()
-            elif ask_menu == 2:
-                search_obj.student_search()
-            else:
-                print("Choose valid option")
+            while True:
+                self.menu_option()
+                ask_menu = int(input("Enter Menu option: "))
+                if ask_menu == 1:
+                    student_obj.student_registration()
+                elif ask_menu == 2:
+                    search_obj.student_search()
+                elif ask_menu == 0:
+                    break
+                else:
+                    print("Choose valid option")
         except Exception as e:
-            update_errorslog(error_details(e),diff_path_obj)
+            file_io_obj.update_errorslog(file_io_obj.get_errdetails(e),diff_path_obj.errorlog_path)
 
 homepage_obj = Homepage()

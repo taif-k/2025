d760fb5e34b7964aa6d9e7274066712467bf0523
taif-k <taifk817@gmail.com>
2025-10-27 10:56:35 +0530

    cmnt corrected

--- python/Programs/name_as_main2.py
+++ python/Programs/name_as_main2.py
@@ -1,6 +1,6 @@
 import name_as_main
 
 
-# if test1 was running directly as script name was "__main__"
-# but here name is test1 
+# if name_as_main was running directly as script name was "__main__"
+# but here name is name_as_main because it is imported and then used 
 print(name_as_main.__name__) 

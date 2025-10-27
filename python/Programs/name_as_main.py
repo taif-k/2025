091d2dd011a8e3146a9ed6f04f8aaa34ec04f65f
taif-k <taifk817@gmail.com>
2025-10-27 10:53:41 +0530

    __name__ == "__main__" TEST

---
+++ python/Programs/name_as_main.py
@@ -0,0 +1,7 @@
+
+
+if __name__ == "__main__":
+    print('Name is __main__')
+    print('This is inside if block')
+
+print('This is outside if block')
---
+++ python/Programs/name_as_main2.py
@@ -0,0 +1,6 @@
+import name_as_main
+
+
+# if test1 was running directly as script name was "__main__"
+# but here name is test1 
+print(name_as_main.__name__) 

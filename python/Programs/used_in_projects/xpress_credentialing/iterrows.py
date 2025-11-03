47a94aca91f9c35147fe108340c5f58f721d084d
taif-k <taifk817@gmail.com>
2025-11-03 18:35:07 +0530

    iterrows(), isinstance() & ast.literal_evall()

--- python/Programs/used_in_projects/xpress_credentialing/iterrows.py
+++ python/Programs/used_in_projects/xpress_credentialing/iterrows.py
@@ -1,6 +1,6 @@
 import pandas as pd, json
 from pandas import json_normalize
-
+                                   # iterrows
 data = [{'created_epoch': '1138390973000', 'enumeration_type': 'NPI-1', 'last_updated_epoch': '1698463706000', 'number': 123, 'addresses': [{'country_code': 'US', 'country_name': 'United States', 'address_purpose': 'MAILING', 'address_type': 'DOM', 'address_1': 'PO BOX 5074', 'city': 'SIOUX FALLS', 'state': 'SD', 'postal_code': '571175074', 'telephone_number': '605-328-9419'}, {'country_code': 'US', 'country_name': 'United States', 'address_purpose': 'LOCATION', 'address_type': 'DOM', 'address_1': '3035 DEMERS AVE', 'city': 'GRAND FORKS', 'state': 'ND', 'postal_code': '582014040', 
 'telephone_number': '701-746-7521', 'fax_number': '701-795-2553'}], 'practiceLocations': [], 'basic': {'first_name': 'DAVID', 'last_name': 'SCHALL', 'middle_name': 'M', 'credential': 'MD', 'sole_proprietor': 'NO', 'sex': 'M', 'enumeration_date': '2006-01-27', 'last_updated': '2023-10-27', 'certification_date': '2023-10-27', 'status': 'A', 'name_prefix': 'Dr.'}, 'taxonomies': [{'code': '207X00000X', 'taxonomy_group': '', 'desc': 'Orthopaedic Surgery', 'state': 'MN', 'license': '41071', 'primary': False}, {'code': '207X00000X', 'taxonomy_group': '', 'desc': 'Orthopaedic Surgery', 'state': 'ND', 'license': '9038', 'primary': True}], 'identifiers': [{'code': '01', 'desc': 'Other (non-Medicare)', 'issuer': 'NDBCBS', 'identifier': '021872', 'state': 'ND'}, {'code': '05', 'desc': 'MEDICAID', 'issuer': None, 'identifier': 
 '11989', 'state': 'ND'}, {'code': '05', 'desc': 'MEDICAID', 'issuer': None, 'identifier': '200001954', 'state': 'MN'}, {'code': '01', 'desc': 'Other (non-Medicare)', 'issuer': 'RR MEDICARE', 'identifier': '200045181', 'state': None}, {'code': '01', 'desc': 'Other (non-Medicare)', 'issuer': 'BCBS MN', 'identifier': '62G05SC', 'state': 'MN'}], 'endpoints': [], 'other_names': []
@@ -34,6 +34,18 @@ for record in df_flatten.itertuples():
     print(record)
  
     
+                                      # isinstance
+py_list = []
+list_as_str = "[]"
+print(isinstance(py_list,list))
+print(isinstance(list_as_str,str))
 
+                                    # ast.literal_eval()
+import ast
+anything_but_string = "[]"
+# print(eval(anything_but_string), type(eval(anything_but_string))) # eval() can run arbitary code also
+
+# safer version 
+print(type(ast.literal_eval(anything_but_string)))
 
 

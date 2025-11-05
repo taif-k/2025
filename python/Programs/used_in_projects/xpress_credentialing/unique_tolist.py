9d9dd77db78090741fa230447e67fe19ce5819dd
taif-k <taifk817@gmail.com>
2025-11-05 08:39:07 +0530

    unique in pandas , set in py cmnt

--- python/Programs/used_in_projects/xpress_credentialing/unique_tolist.py
+++ python/Programs/used_in_projects/xpress_credentialing/unique_tolist.py
@@ -3,7 +3,7 @@ import pandas as pd
 data = [{'name': 'abc'},{'name': 'abc'}]
 a = pd.DataFrame(data)
 
-# unique() in pandas is similar to set() in python
+# unique() in pandas is similar to set() in python - unique keeps appearance order unlike set 
 print(a['name'].unique(), type(a['name'].unique()), a['name'].dtype)
 print(a['name'].unique().tolist(), type(a['name'].unique().tolist()))
 # print(a['name'].unique().tolist().dtype) # now series has become list so dtype wont work on it  

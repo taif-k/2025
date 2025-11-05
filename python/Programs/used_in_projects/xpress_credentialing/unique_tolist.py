7dd1eff54ae3dd86341853279cce5bb541c94435
taif-k <taifk817@gmail.com>
2025-11-05 08:36:19 +0530

    unique() & tolist()

---
+++ python/Programs/used_in_projects/xpress_credentialing/unique_tolist.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+data = [{'name': 'abc'},{'name': 'abc'}]
+a = pd.DataFrame(data)
+
+# unique() in pandas is similar to set() in python
+print(a['name'].unique(), type(a['name'].unique()), a['name'].dtype)
+print(a['name'].unique().tolist(), type(a['name'].unique().tolist()))
+# print(a['name'].unique().tolist().dtype) # now series has become list so dtype wont work on it  

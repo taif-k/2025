ae7d4152b18e23c0d58e058e5c87f15a8d0f54a9
taif-k <taifk817@gmail.com>
2025-11-08 10:28:35 +0530

    multi named exports are imported but default is single import

--- frontend/react/vite-test-app/src/main.jsx
+++ frontend/react/vite-test-app/src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import {App,Txt} from './App.jsx' // when doing default import(it can be named anything)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Txt/>
+    <App/>
   </StrictMode>,
 )

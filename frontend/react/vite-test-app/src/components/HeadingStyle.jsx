96eed5629df777fb9e5e707b3b3df7f3dd832d98
taif-k <taifk817@gmail.com>
2025-11-12 16:40:37 +0530

    testing BS

---
+++ frontend/react/vite-test-app/src/components/HeadingStyle.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeadingStyle = () => {
+    return (
+        <>
+            <h1 className="p-5 bg-primary text-white">This is your heading 1</h1 >
+            <h3 className="p-5 bg-secondary text-white">This is your sub heading</h3>
+            <button className='btn btn-primary'>Submit</button>
+            
+        </>
+    );
+};
+export default HeadingStyle

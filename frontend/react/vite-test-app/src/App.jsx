4f57935130c6640e3fdde33047111fac2838369b
taif-k <taifk817@gmail.com>
2025-11-12 09:17:53 +0530

    comp reused

--- frontend/react/vite-test-app/src/App.jsx
+++ frontend/react/vite-test-app/src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css'
-import Profile from './components/Profile'
+import Profile, { Avatar } from './components/Profile'
+import williamjohnimg from './assets/williamjohn.PNG'
+import williamimg from './assets/william.PNG'
+import noNameImg from './assets/noName.PNG'
 
 export function App() {
 
   return (
     <>
-    <Profile Name='Kevin Williams' Email='kevinwilliams@mail.com' Phone={1234567890} />
+    <Profile Name='Williams' Email='kevinwilliams@gmail.com' Phone="810-999-3292" imgg = {williamjohnimg}/>
+    <Profile Name='Kevin Williams' Email='kevinwilliams@mail.com' Phone="810-858-3292" imgg = {williamimg}/>
+    <div className="avatar-left">
+      <Avatar imgg={noNameImg} />
+    </div>
     </>
   )
 }
--- frontend/react/vite-test-app/src/components/Profile.jsx
+++ frontend/react/vite-test-app/src/components/Profile.jsx
@@ -1,47 +1,42 @@
 import kevinPhoto from '../assets/kevin_willi_table_left.PNG';
-import '../app.css'; 
+import '../app.css';
 
-const Profile = ({ Name = "Taif", Email = "Test@mail.com", Phone = 1234567890 }) => {
+const Profile = (props) => {
+const { Name = "Taif", Email = "Test@mail.com", Phone = 1234567890, imgg = kevinPhoto  } = props
   return (
-    <table className="profile-table">
-      <tbody>
-        <tr>
-          <td rowSpan="3" className="profile-photo-cell">
-            <img src={kevinPhoto} alt="Profile" className="profile-photo" />
-          </td>
-          <td className="profile-label">Name</td>
-          <td>{Name}</td>
-        </tr>
-        <tr>
-          <td className="profile-label">Email</td>
-          <td>{Email}</td>
-        </tr>
-        <tr>
-          <td className="profile-label">Phone</td>
-          <td>{Phone}</td>
-        </tr>
-      </tbody>
-    </table>
+    <>
+      <table className="profile-table">
+        <tbody>
+          <tr>
+            <td rowSpan="3" className="profile-photo-cell">
+              <img src={imgg} alt="Profile" className="profile-photo" />
+            </td>
+            <td className="profile-label">Name</td>
+            <td>{Name}</td>
+          </tr>
+          <tr>
+            <td className="profile-label">Email</td>
+            <td>{Email}</td>
+          </tr>
+          <tr>
+            <td className="profile-label">Phone</td>
+            <td>{Phone}</td>
+          </tr>
+        </tbody>
+      </table>
+      {!Name && <Avatar imgg={imgg} />}
+    </>
+
   );
 };
 
 export default Profile;
 
 
-// export Txt = ()=> {return "hi"}
-export const Txt = ()=>{
-  const use_variable = 'string in variable within {}'
-  const use_function = () => 'calling function in {}'
+export const Avatar = ({imgg}) => {
   return (
     <>
-    Variable: {use_variable}<br/>
-    Function: {use_function()}<br/>
-    Arth: {"2" - 2}<br/>
-    Ternry: {2>1?'true':'false'}<br/>
-    Map : {["seven","three","eight"].map((string)=>{
-      return string + " "
-    })}
-    <p>test p tag for multiple named export</p>
+      <img src={imgg} alt="Avatar" className="avatar-photo" />
     </>
   )
-}
+} 

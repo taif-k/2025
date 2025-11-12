4cbb3ad9cbdf195811355927f9b067f9b41fbb63
taif-k <taifk817@gmail.com>
2025-11-12 16:38:55 +0530

    more comp for testing

--- frontend/react/vite-test-app/src/App.jsx
+++ frontend/react/vite-test-app/src/App.jsx
@@ -3,16 +3,22 @@ import Profile, { Avatar } from './components/Profile'
 import williamjohnimg from './assets/williamjohn.PNG'
 import williamimg from './assets/william.PNG'
 import noNameImg from './assets/noName.PNG'
+import HeadingStyle from './components/HeadingStyle'
+import UserStatus from './components/UserStatus'
+import ToDoList from './components/ToDoList'
 
 export function App() {
 
   return (
     <>
-    <Profile Name='Williams' Email='kevinwilliams@gmail.com' Phone="810-999-3292" imgg = {williamjohnimg}/>
-    <Profile Name='Kevin Williams' Email='kevinwilliams@mail.com' Phone="810-858-3292" imgg = {williamimg}/>
+    <ToDoList/>
+    <UserStatus status={"false"}/>
+    {/* <HeadingStyle/> */}
+    {/* <Profile Name='William John' Email='williamjohn@gmail.com' Phone="810-999-3292" imgg = {williamjohnimg}/>
+    <Profile Name='Williams' Email='kevinwilliams@gmail.com' Phone="810-858-3292" imgg = {williamimg}/>
     <div className="avatar-left">
       <Avatar imgg={noNameImg} />
-    </div>
+    </div> */}
     </>
   )
 }

f0ac4869ee4762e002725153f1033cdc9e051f3b
taif-k <taifk817@gmail.com>
2025-11-14 20:08:46 +0530

    useReducer to inc/decrement

--- frontend/react/vite-test-app/src/App.jsx
+++ frontend/react/vite-test-app/src/App.jsx
@@ -6,19 +6,30 @@ import noNameImg from './assets/noName.PNG'
 import HeadingStyle from './components/HeadingStyle'
 import UserStatus from './components/UserStatus'
 import ToDoList from './components/ToDoList'
+import Count from './components/Count'
+import CountWithReducer from './components/CountWithReducer'
 
 export function App() {
 
   return (
     <>
-    <ToDoList/>
-    <UserStatus status={"false"}/>
-    <HeadingStyle/>
-    <Profile Name='William John' Email='williamjohn@gmail.com' Phone="810-999-3292" imgg = {williamjohnimg}/>
+        <p>useReducer()</p>
+    <CountWithReducer/>
+
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <p>useState()</p>
+    <Count/>
+    {/* <ToDoList/>  */}
+    {/* <UserStatus status={"false"}/> */}
+    {/* <HeadingStyle/> */}
+    {/* <Profile Name='William John' Email='williamjohn@gmail.com' Phone="810-999-3292" imgg = {williamjohnimg}/>
     <Profile Name='Williams' Email='kevinwilliams@gmail.com' Phone="810-858-3292" imgg = {williamimg}/>
     <div className="avatar-left">
       <Avatar imgg={noNameImg} />
-    </div>
+    </div> */}
     </>
   )
 }

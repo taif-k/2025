57da22f41d5b8920a3593531c0831ffe9f60da89
taif-k <taifk817@gmail.com>
2025-11-20 21:52:06 +0530

    using declarative mode

--- frontend/react/vite-test-app/src/App.jsx
+++ frontend/react/vite-test-app/src/App.jsx
@@ -1,34 +1,15 @@
 import './App.css'
-import Profile, { Avatar } from './components/Profile'
-import williamjohnimg from './assets/williamjohn.PNG'
-import williamimg from './assets/william.PNG'
-import noNameImg from './assets/noName.PNG'
-import HeadingStyle from './components/HeadingStyle'
-import UserStatus from './components/UserStatus'
-import ToDoList from './components/ToDoList'
-import Count from './components/Count'
-import CountWithReducer from './components/CountWithReducer'
+import { BrowserRouter } from 'react-router-dom'
+import DeclarativeMode from './routes/DeclarativeMode'
+
 
 export function App() {
 
   return (
     <>
-        <p>useReducer()</p>
-    <CountWithReducer/>
-
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <p>useState()</p>
-    <Count/>
-    <UserStatus status={"false"}/>
-    <HeadingStyle/>
-   <Profile Name='William John' Email='williamjohn@gmail.com' Phone="810-999-3292" imgg = {williamjohnimg}/>
-    <Profile Name='Williams' Email='kevinwilliams@gmail.com' Phone="810-858-3292" imgg = {williamimg}/>
-    <div className="avatar-left">
-      <Avatar imgg={noNameImg} />
-    </div>
+      <BrowserRouter>
+        <DeclarativeMode/>
+      </BrowserRouter>
     </>
   )
 }

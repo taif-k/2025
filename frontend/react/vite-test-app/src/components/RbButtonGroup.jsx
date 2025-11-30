4ed0ebf3b5fe55bb536fd8f3707c72118e72dc3d
taif-k <taifk817@gmail.com>
2025-11-30 21:40:02 +0530

    react button

--- frontend/react/vite-test-app/src/components/RbButtonGroup.jsx
+++ frontend/react/vite-test-app/src/components/RbButtonGroup.jsx
@@ -1,14 +1,26 @@
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
 
 const RbButtonGroup = () => {
+  const [align, setAlign] = useState("right")
+  const getTextDirection = () => {
+    return {
+      textAlign: align,
+    }
+  }
   return (
     <>
-      <ButtonGroup aria-label="Basic example">
-      <Button variant="secondary">Left</Button>
-      <Button variant="secondary">Middle</Button>
-      <Button variant="secondary">Right</Button>
-    </ButtonGroup>
+        <ButtonGroup aria-label="Basic example">
+          <Button variant="secondary d-flex align-items-center" onClick={() => setAlign("left")}>Left</Button>
+          <Button variant="secondary d-flex align-items-center" onClick={() => setAlign("center")}>Center</Button>
+          <Button variant="secondary d-flex align-items-center" onClick={() => setAlign("right")}>Right</Button>
+        </ButtonGroup>
+        <p className='mt-2' style={getTextDirection()}>
+          Here, the actions of the above buttons will be reflected
+        </p>
+      
     </>
   )
 }

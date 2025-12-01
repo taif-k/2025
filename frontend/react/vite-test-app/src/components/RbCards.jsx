50ff6a17e438071bc4a98c43812d87dedbd3136b
taif-k <taifk817@gmail.com>
2025-12-01 20:11:32 +0530

    sample description & img for cards added

--- frontend/react/vite-test-app/src/components/RbCards.jsx
+++ frontend/react/vite-test-app/src/components/RbCards.jsx
@@ -7,30 +7,32 @@ const RbCards = () => {
     {
       id: 1,
       name: "Bluetooth Headphones",
-      description: "simple text 1 ",
+      description: "Boult Newly flex on ear bluetooth headphones, C type fast charging Boult Newly flex on ear bluetooth headphones, C type fast charging",
       price: "1399",
       mrp: "3990",
+      img: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     },
     {
       id: 2,
       name: "The Ear Gaming Headphones",
-      description: "simple text 2",
+      description: "Boult Newly flex on ear bluetooth headphones, C type fast charging Boult Newly flex on ear bluetooth headphones, C type fast charging",
       price: "1399",
       mrp: "3990",
+      img: "https://media.istockphoto.com/id/835148968/photo/red-headphones-isolated.jpg?s=612x612&w=0&k=20&c=JAEd1MYVaJjC0Iu1cZ4LPHRigRGZ-NJNjIXXs87me1E="
     },
   ]
 
 
   return (
     headphones.map((headphone) => (
-      <Card style={{ width: '18rem' }}>
+      <Card style={{ width: '25rem' }}>
         <Card.Title className='mt-2 ps-2'>{headphone.name}</Card.Title>
-        <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+        <Card.Img variant="top" src={headphone.img} />
         <Card.Body>
           <Card.Text>
             {headphone.description}
           </Card.Text>
-          <p>{headphone.price}<span className='ms-2 text-decoration-line-through'>{headphone.mrp}</span></p>
+          <p>{headphone.price}<span className='ms-2 text-decoration-line-through'>MRP {headphone.mrp}</span></p>
         </Card.Body>
         <Card.Footer className='d-flex justify-content-between'>
           <Button variant='outline-primary' size='sm'>Add to Cart</Button>

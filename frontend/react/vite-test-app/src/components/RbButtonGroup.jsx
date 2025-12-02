0e1ef133f0ba29700a22c82f17d2b3bce696ce76
taif-k <taifk817@gmail.com>
2025-12-02 20:12:59 +0530

    using map() instead manual create btn

--- frontend/react/vite-test-app/src/components/RbButtonGroup.jsx
+++ frontend/react/vite-test-app/src/components/RbButtonGroup.jsx
@@ -1,28 +1,37 @@
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
 
 const RbButtonGroup = () => {
-  const [align, setAlign] = useState("right")
-  const getTextDirection = () => {
-    return {
-      textAlign: align,
-    }
-  }
+  const [align, setAlign] = useState("center");
+
+  const buttonOptions = [
+    { label: "Left", value: "left" },
+    { label: "Center", value: "center" },
+    { label: "Right", value: "right" }
+  ];
+
   return (
     <>
-        <ButtonGroup aria-label="Basic example">
-          <Button variant="secondary d-flex align-items-center" onClick={() => setAlign("left")}>Left</Button>
-          <Button variant="secondary d-flex align-items-center" onClick={() => setAlign("center")}>Center</Button>
-          <Button variant="secondary d-flex align-items-center" onClick={() => setAlign("right")}>Right</Button>
-        </ButtonGroup>
-        <p className='mt-2' style={getTextDirection()}>
-          Here, the actions of the above buttons will be reflected
-        </p>
-      
-    </>
-  )
-}
+      <ButtonGroup aria-label="Alignment Buttons">
+        {buttonOptions.map((btn, index) => (
+          <Button
+            key={index}
+            variant="secondary d-flex align-items-center"
+            onClick={() => setAlign(btn.value)}
+          >
+            {btn.label}
+          </Button>
+        ))}
+      </ButtonGroup>
 
-export default RbButtonGroup
+      <div className="w-100">
+        <p className="mt-2" style={{ textAlign: align }}>
+          Sample Text
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default RbButtonGroup;

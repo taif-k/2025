732b8228e531925c3aff10011988faeb9664959e
taif-k <taifk817@gmail.com>
2025-12-05 10:52:04 +0530

    index.css path updated

--- frontend/react/vite-test-app/src/main.jsx
+++ frontend/react/vite-test-app/src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './assets/style/index.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import App from './App'
 

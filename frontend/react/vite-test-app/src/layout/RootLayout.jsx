286900ee3a4e121661b6ecd2a82d230cf05f04ce
taif-k <taifk817@gmail.com>
2025-12-05 22:07:34 +0530

    using Row and Col

--- frontend/react/vite-test-app/src/layout/RootLayout.jsx
+++ frontend/react/vite-test-app/src/layout/RootLayout.jsx
@@ -1,44 +1,87 @@
-import { Outlet, NavLink } from 'react-router-dom'
+import { Outlet, NavLink } from 'react-router-dom';
+import RbBreadCrumb from '../components/RbBreadCrumbs';
+import Accordion from 'react-bootstrap/Accordion';
+import Button from 'react-bootstrap/Button';
+import { ArrowRight, Columns } from "react-bootstrap-icons";
+import { Container, Row, Col } from 'react-bootstrap';
+
+
+
+const nonInteractiveLinks = [
+  { path: "/badges", label: "Badges" },
+  { path: "/breadcrumb", label: "Breadcrumbs" },
+  { path: "/buttons", label: "Buttons" },
+  { path: "/buttongroups", label: "ButtonGroup" },
+  { path: "/cards", label: "Card" },
+  { path: "/images", label: "Images" },
+  { path: "/listgroup", label: "List Groups" },
+  { path: "/figures", label: "Figures" },
+  { path: "/pagination", label: "Pagination" },
+  { path: "/progressbar", label: "Progress Bars" },
+  { path: "/spinner", label: "Spinners" },
+  { path: "/table", label: "Tables" },
+  { path: "/closebutton", label: "Close Buttons" }
+];
+
+const interactiveLinks = [
+  { path: "/accordion", label: "Accordion" },
+  { path: "/crousel", label: "Crousel" },
+  { path: "/dropdown", label: "Dropdown" },
+  { path: "/modal", label: "Modal" },
+  { path: "/navbar-offcanvas", label: "Navbar Offcanvas" },
+  { path: "/navtab", label: "Navtab" },
+  { path: "/overlay", label: "Overlay" }
+];
 
 const RootLayout = () => {
-    return (
-        <div className='container'>
-            <div className="row">
-                {/* <div className="col-md-3">
-                    Navigation
-                    <ul>
-                        <li><NavLink to='/'>Home</NavLink></li>
-                        <li><NavLink to='/profile'>Profile</NavLink></li>
-                        <li><NavLink to='/todo' >Todo List</NavLink></li>
-                        <li><NavLink to='/usereduce' >useReducer</NavLink></li>
-                        <li><NavLink to='/usestate' >useState</NavLink></li>
-                        <li><NavLink to='/reactbootstrap' >React Bootstrap</NavLink></li>
-                    </ul>
-                </div> */}
-                <div className="col-md-3">
-                    Components
-                    <ul>
-                        <li><NavLink to='/badges'>Badges</NavLink></li>
-                        <li><NavLink to='/breadcrumbs'>Breadcrumbs</NavLink></li>
-                        <li><NavLink to='/buttons' >Buttons</NavLink></li>
-                        <li><NavLink to='/buttongroups' >ButtonGroup</NavLink></li>
-                        <li><NavLink to='/cards' >Card</NavLink></li>
-                        <li><NavLink to='/images' >Images</NavLink></li>
-                        <li><NavLink to='/listgroup'>List Groups</NavLink></li>
-                        <li><NavLink to='/figures'>Figures</NavLink></li>
-                        <li><NavLink to='/pagination' >Pagination</NavLink></li>
-                        <li><NavLink to='/progressbar' >Progress Bars</NavLink></li>
-                        <li><NavLink to='/spinner' >Spinners</NavLink></li>
-                        <li><NavLink to='/table' >Tables</NavLink></li>
-                        <li><NavLink to='/closebutton' >Close Buttons</NavLink></li>
-                    </ul>
-                </div>
-                <div className='col-md-9 border border-1'>
-                    <Outlet />
-                </div>
-            </div>
-        </div>
-    )
-}
+  return (
 
-export default RootLayout
+    <Container>
+      <Row>
+        <Col className="p-3 border-end" md={3}>
+          <Button variant="primary" className="mb-2">
+            Bootstrap Components
+          </Button>
+
+          <Accordion defaultActiveKey="0">
+            <Accordion.Item eventKey="0">
+              <Accordion.Header>Non Interactive</Accordion.Header>
+              <Accordion.Body>
+                <ul className="list-unstyled">
+                  {nonInteractiveLinks.map((item, index) => (
+                    <li key={index} className="sidebar-link">
+                      <ArrowRight className="me-2 icon-blue" />
+                      <NavLink to={item.path}>{item.label}</NavLink>
+                    </li>
+                  ))}
+                </ul>
+              </Accordion.Body>
+            </Accordion.Item>
+
+
+            <Accordion.Item eventKey="1">
+              <Accordion.Header>Interactive</Accordion.Header>
+              <Accordion.Body>
+                <ul className="list-unstyled">
+                  {interactiveLinks.map((item, index) => (
+                    <li key={index} className="sidebar-link">
+                      <ArrowRight className="me-2 icon-blue" />
+                      <NavLink to={item.path}>{item.label}</NavLink>
+                    </li>
+                  ))}
+                </ul>
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+        </Col>
+
+        <Col md={9}>
+          <RbBreadCrumb />
+          <Outlet />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default RootLayout;

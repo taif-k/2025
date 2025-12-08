033dcb4523d05c7f5d3708b1a3b1d6403de980a1
taif-k <taifk817@gmail.com>
2025-12-08 16:01:12 +0530

    react bars with a card added

--- frontend/react/vite-test-app/src/components/RbProgressBar.jsx
+++ frontend/react/vite-test-app/src/components/RbProgressBar.jsx
@@ -1,10 +1,46 @@
+import { useState } from 'react';
 import ProgressBar from 'react-bootstrap/ProgressBar';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Badge from 'react-bootstrap/Badge';
+
 
 const RbProgressBar = () => {
-    const now = 60;
+    const [value, setValue] = useState(0)
+    const handleIncrease = () => setValue(value + 5)
+    const handleDecrease = () => setValue(value - 5)
+
     return (
         <>
-            <ProgressBar now={now} label={`${now}%`} />
+            <div className="text-start w-100">
+                <Button onClick={handleIncrease} className='me-2'>Progress 5+</Button>
+                <Button onClick={handleDecrease}>Progress 5-</Button>
+
+                <p className='fw-bold mt-2 pt-2'>Completed {value}%</p>
+
+                <ProgressBar now={value} label={`${value}%`} striped animated variant='success' className="mb-2" />
+                <ProgressBar now={value} striped variant='danger' />
+            </div>
+
+            <Card style={{ width: '18rem' }} className='m-3'>
+                <Card.Body className="text-start">
+                    <Card.Title>Bootstrap Dashboard Application</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">Web Development</Card.Subtitle>
+                    <Card.Text>
+                        Some quick example text to build on the card title
+                    </Card.Text>
+                    <Badge bg='info'>In Progress</Badge>
+                    <ProgressBar label='5%' className="my-2" />
+
+
+                    <div className="d-flex gap-3 mt-2">
+                        <Card.Text>Due Date: 1 Jan, 2022</Card.Text>
+                        <Card.Text>Budget: $123,000</Card.Text>
+                    </div>
+                </Card.Body>
+            </Card>
+
+
         </>
     )
 }

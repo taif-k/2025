7a6a43944685f84b5e5cae25ca16b8d4263c5702
taif-k <taifk817@gmail.com>
2025-12-21 21:10:18 +0530

    wishlistprovider added

--- frontend/react/vite-test-app/src/App.jsx
+++ frontend/react/vite-test-app/src/App.jsx
@@ -3,12 +3,15 @@ import { BrowserRouter, RouterProvider } from 'react-router-dom'
 import {router} from './routes/DataMode'
 import DeclarativeMode from './routes/DeclarativeMode'
 import UserProvider from './provider/UserProvider'
+import WishListProvider from './provider/WishListProvider'
 
 function App() {
 
   return (
     <UserProvider>
+      <WishListProvider>
         <RouterProvider router={router} />
+      </WishListProvider>
     </UserProvider>
     // <>
     //   <BrowserRouter>
